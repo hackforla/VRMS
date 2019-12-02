225184ecbe00eaeaa713f974857630f3f91f4d7e
sarL3y <matt.tapper@gmail.com>
2019-12-02 15:51:11 -0800

    Docker integration

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const app = require('./index.js');
 
 // Endpoint for requests
-const PORT = 3000;
+const PORT = 4000;
 
 // Spin up the server
 app.listen(process.env.PORT || PORT, () => {
-    console.log('Server is listening on port: ' + PORT);
+    console.log('Server is listening on port: ' + process.env.PORT || PORT);
 });
