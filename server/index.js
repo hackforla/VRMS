98c0acb63feb2b9e7880f51afb871b2d99cb621d
sarL3y <matt.tapper@gmail.com>
2019-12-02 21:43:02 -0800

    Fix Express routing

--- server/index.js
+++ server/index.js
@@ -26,11 +26,11 @@ app.use(helmet());
 app.use(cors());
 
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, '/../client/build')));
 
 // Anything that doesn't match the above, send back index.html
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+    res.sendFile(path.join(__dirname, '/../client/build/index.html'));
   });
 
 const Users = require('./users/users.js');
