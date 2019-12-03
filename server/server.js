9ea1bf06a4bc26eaec8ff18574599913ff0f0806
sarL3y <matt.tapper@gmail.com>
2019-12-02 20:43:38 -0800

    Fix Express routing

--- server/server.js
+++ server/server.js
@@ -5,5 +5,5 @@ const PORT = 4000;
 
 // Spin up the server
 app.listen(process.env.PORT || PORT, () => {
-    console.log('Server is listening on port: ' + process.env.PORT || PORT);
+    console.log('Server is listening on port: ' + PORT);
 });
