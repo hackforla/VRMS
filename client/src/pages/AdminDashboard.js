acbde080caf8aade6087714c21263a7c566dfb89
sarL3y <matt.tapper@gmail.com>
2020-01-16 16:59:59 -0800

    Setup Dashboard, enable test connection string

--- client/src/pages/AdminDashboard.js
+++ client/src/pages/AdminDashboard.js
@@ -1,9 +1,14 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 // import { Link } from 'react-router-dom';
 
 import DashboardEvents from '../components/DashboardEvents';
 
+import '../sass/Dashboard.scss';
+
 const AdminDashboard = (props) => {
+    const [events, setEvents] = useState([]);
+    const [users, setUsers] = useState([]);
+    const [tabSelected, setTabSelected] = useState("events");
 
     useEffect(() => {
         // fetchData();
@@ -13,6 +18,52 @@ const AdminDashboard = (props) => {
     return (
         <div className="flex-container">
             <div className="dashboard">
+                {/* <div className="dashboard-nav">
+                    {events && users ? (
+                        <div className="dashboard-nav-row">
+                            <button>
+                                Events
+                            </button>
+                            <button>
+                                Users
+                            </button>
+                        </div>
+                    ) : (
+                        <div className="dashboard-nav-row block">
+                            <button>
+                                Loading...
+                            </button>
+                            <button>
+                                Loading...
+                            </button>
+                        </div> 
+                    )}
+                    
+                    {eventsIsSelected ? (
+                        <div className="dashboard-nav-row">
+                            <button>
+                                Upcoming
+                            </button>
+                            <button>
+                                Past
+                            </button>
+                        </div>
+                    ) : (
+                        null
+                    )}
+
+                    {usersIsSelected ? (
+                        <div className="dashboard-nav-row">
+
+                        </div>
+                    ) : (
+                        null
+                    )}
+                   
+                    <div key={`${event._id}-${index}`} className="home-buttons">
+                        
+                    </div>
+                </div> */}
                 <div className="events">
                     <h3>Your Events:</h3>
                     <DashboardEvents />
