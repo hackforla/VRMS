25f1f91713a8d861c5b3d644f339e93dd44a938f
sarL3y <matt.tapper@gmail.com>
2020-01-21 11:34:15 -0800

    Update server.js for Tuesday

--- client/src/pages/AdminLogin.js
+++ client/src/pages/AdminLogin.js
@@ -1,43 +1,87 @@
-import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
 
+import useAuth from '../hooks/useAuth';
+import { authContext } from '../context/authContext';
 
-// import '../sass/HomeContainer.scss';
+import '../sass/AdminLogin.scss';
 // import '../sass/HomeContainer-media-queries.scss';
 
+
+
 const AdminLogin = (props) => {
+
     // const [isLoading, setIsLoading] = useState(false);
     // const [event, setEvent] = useState([]);
-    // const [isError, setIsError] = useState(null);
+    const [isError, setIsError] = useState(false);
+    const [email, setEmail] = useState("");
+    const auth = useAuth();
 
-    // async function fetchData() {
-    //     try {
-    //         const res = await fetch(`http://localhost:4000/api/events/${props.match.params.id}`);
-    //         const resJson = await res.json();
-    //         setEvent(resJson);
-    //     } catch(error) {
-    //         setIsError(error);
-    //         alert(error);
-    //     }
-    // }
+    const handleInputChange = (e) => setEmail(e.currentTarget.value);
 
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        
+        setIsError(false);
+        try {
+            const isAdmin = await auth.login(email);
+
+            if(isAdmin) {
+                console.log('handleLogin worked!');
+                props.history.push('/admin');
+            } else {
+                setIsError(true);
+                console.log('Welp that didnt work...');
+            }
+        } catch(error) {
+            console.log(error);
+        }
+        
+    };
+    
     useEffect(() => {
-        // fetchData();
 
     }, []);
 
     return (
-        <div className="flexcenter-container">
-            <div className="adminlogin">
+        <div className="flex-container">
+            <div className="adminlogin-container">
                 <div className="adminlogin-headers">
                     <h3>Welcome Back!</h3>
-                    <h4>Please login below.</h4>
                 </div>
-                <div className="adminlogin-buttons">
-                    <Link to={'/login'}>Login</Link>
+                    <form className="form-check-in" autoComplete="off" onSubmit={e => e.preventDefault()}>
+                        <div className="form-row">
+                            <div className="form-input-text">
+                                <label htmlFor="email">Enter your email address:</label>
+                                <input 
+                                    type="email"
+                                    name="email"
+                                    placeholder="Email Address"
+                                    value={email.toString()}
+                                    // aria-label="topic"
+                                    onChange={handleInputChange}
+                                    aria-label="Email Address"
+                                    autoComplete="none"
+                                    required
+                                /> 
+                            </div>
+                        </div>
+                    </form>
+                    <div className="adminlogin-warning">
+                        {isError ? `You can try entering your email again, but you might not have access to a dashboard yet` : null}
+                    </div>
+
+                    <div className="form-input-button">
+                        <button
+                            onClick={e => handleLogin(e)}
+                            className="login-button"
+                        >
+                            LOGIN
+                        </button>
+                    </div>
+                    
                 </div>
             </div>
-        </div>
     )
 };
 
