f48db65e32bdad8bc8f5a007cbfb7eca30cd92a3
sarL3y <matt.tapper@gmail.com>
2020-02-19 16:47:47 -0800

    Adds SVG icons per design requirements

--- client/src/pages/AdminDashboard.js
+++ client/src/pages/AdminDashboard.js
@@ -6,6 +6,8 @@ import DashboardEvents from '../components/DashboardEvents';
 import DashboardUsers from '../components/DashboardUsers';
 
 import useAuth from '../hooks/useAuth';
+import { ReactComponent as ClockIcon} from '../svg/Icon_Clock.svg';
+import { ReactComponent as LocationIcon} from '../svg/Icon_Location.svg';
 
 import '../sass/Dashboard.scss';
 
@@ -28,7 +30,7 @@ const AdminDashboard = (props) => {
     const [usersIsSelected, setUsersIsSelected] = useState(false);
     const [isLoading, setIsLoading] = useState(false);
 
-    const auth = useAuth();
+    // const auth = useAuth();
 
     async function getAndSetBrigadeEvents() {
         try {
@@ -238,7 +240,7 @@ const AdminDashboard = (props) => {
     }, []);
 
     return (
-        auth.user ? (
+        // auth.user ? (
             <div className="flex-container">
                 <div className="dashboard">
                     <div className="dashboard-header">
@@ -249,8 +251,14 @@ const AdminDashboard = (props) => {
                         <div className="warning-event">
                             <div className="warning-event-headers">
                                 <p className="event-name">{nextEvent[0].name}</p>
-                                <p className="event-info">{nextEvent[0].date}</p>
-                                <p className="event-info">{nextEvent[0].location.city}, {nextEvent[0].location.state}</p>
+                                <div className="event-info-wrapper">
+                                    <ClockIcon />
+                                    <p className="event-info">{nextEvent[0].date}</p>
+                                </div>
+                                <div className="event-info-wrapper">
+                                    <LocationIcon />
+                                    <p className="event-info">{nextEvent[0].location.city}, {nextEvent[0].location.state}</p>
+                                </div>
                             </div>
                             <div className="warning-event-toggle">
                                 {nextEvent[0] && isCheckInReady === false ? 
@@ -393,9 +401,9 @@ const AdminDashboard = (props) => {
                         </div>  */}
                 </div>
             </div>
-        ) : (
-            <Redirect to="/login" />
-        )
+        // ) : (
+        //     <Redirect to="/login" />
+        // )
     )
 };
 
