e0635cece128ec808334d83808c4dddd0b48743a
sarL3y <matt.tapper@gmail.com>
2020-02-19 22:58:39 -0800

    Reinstate auth for live

--- client/src/pages/AdminDashboard.js
+++ client/src/pages/AdminDashboard.js
@@ -253,7 +253,7 @@ const AdminDashboard = (props) => {
     }, []);
 
     return (
-        // auth.user ? (
+        auth.user ? (
             <div className="flex-container">
                 <div className="dashboard">
                     <div className="dashboard-header">
@@ -416,9 +416,9 @@ const AdminDashboard = (props) => {
                         </div>  */}
                 </div>
             </div>
-        // ) : (
-        //     <Redirect to="/login" />
-        // )
+        ) : (
+            <Redirect to="/login" />
+        )
     )
 };
 
