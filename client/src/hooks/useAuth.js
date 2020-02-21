69eadc84ac14e42c59888b2e7cc5843d56c6ea45
sarL3y <matt.tapper@gmail.com>
2020-02-21 13:04:33 -0800

    Fix dev build issues

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 
-import { AuthContext } from '../context/AuthContext';
+import { AuthContext } from '../context/authContext';
 
 const useAuth = () => {
     return useContext(AuthContext);
