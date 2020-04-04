8bba352aa65e947a57130c3e360e1319ee83fe31
sarL3y <matt.tapper@gmail.com>
2020-04-04 13:27:26 -0700

    Adds node-fetch and sort events working

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const bodyParser = require("body-parser");
 // const cors = require('cors');
 const path = require("path");
 const cron = require("node-cron");
+const fetch = require("node-fetch");
 
 // Create a new application using the Express framework
 const app = express();
@@ -34,7 +35,7 @@ app.use(morgan("dev"));
 mongoose.Promise = global.Promise;
 
 // Workers
-const openCheckinWorker = require('./workers/openCheckins')(cron);
+const runOpenCheckinWorker = require('./workers/openCheckins')(cron, fetch);
 
 // ROUTES
 const eventsRouter = require("./routers/events.router");
@@ -67,39 +68,41 @@ app.get("*", (req, res) => {
 let server;
 
 async function runServer(databaseUrl, port = PORT) {
-  await mongoose
-    .connect(databaseUrl, {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false
-    })
-    .catch(err => err);
+	await mongoose
+		.connect(databaseUrl, {
+			useNewUrlParser: true,
+			useCreateIndex: true,
+			useUnifiedTopology: true,
+			useFindAndModify: false
+		})
+		.catch(err => err);
 
-  server = app
-    .listen(port, () => {
-      console.log(
-        `Mongoose connected from runServer() and is listening on ${port}`
-      );
-    })
-    .on("error", err => {
-      mongoose.disconnect();
-      return err;
-    });
+	server = app
+		.listen(port, () => {
+			console.log(
+				`Mongoose connected from runServer() and is listening on ${port}`
+			);
+		})
+		.on("error", err => {
+			mongoose.disconnect();
+			return err;
+		});
 }
 
 async function closeServer() {
-  await mongoose.disconnect().then(() => {
-    return new Promise((resolve, reject) => {
-      console.log("Closing Mongoose connection. Bye");
-      server.close(err => {
-        if (err) {
-          return reject(err);
-        }
-        resolve();
-      });
-    });
-  });
+	await mongoose.disconnect().then(() => {
+		return new Promise((resolve, reject) => {
+			console.log("Closing Mongoose connection. Bye");
+
+			server.close(err => {
+				if (err) {
+					return reject(err);
+				}
+
+				resolve();
+			});
+		});
+	});
 }
 
 if (require.main === module) {
--- workers/openCheckins.js
+++ workers/openCheckins.js
@@ -1,7 +1,71 @@
-module.exports = function (cron) {
-    const task = cron.schedule('*/10 * * * * *', () => {
-        console.log("I'm working here!");
+module.exports = (cron, fetch) => {
+
+    // Check to see if any events are about to start, and if so, 
+    // open their respective check-ins
+    
+    async function fetchEvents() {
+        console.log("\n Fetching events \n");
+    
+        try {
+            const res = await fetch("http://localhost:4000/api/events");
+            const resJson = await res.json();
+
+            return resJson;
+        } catch(error) {
+            console.log(error);
+        };
+    };
+
+    async function sortAndFilterEvents(currentTime, thirtyMinutes) {
+        console.log("\n Sorting events \n");
+
+        const events = await fetchEvents();
+
+        // Filter events if event date is after now but before thirty minutes from now
+        const sortedEvents = events.filter(event => {
+            return (event.date > currentTime) && (event.date < thirtyMinutes);
+        })
+
+        return sortedEvents;
+    };
+
+    async function openCheckins(events) {
+        events.forEach(event => {
+            console.log('Opening event: ', event);
+        })
+    }
+
+    // Open the events for check-in
+    // async function fetchEvents() {
+    //     console.log("I'm working");
+    
+    //     try {
+    //         const res = await fetch("http://localhost:4000/api/events");
+    //         const resJson = await res.json();
+    //         console.log(resJson);
+    //     } catch(error) {
+    //         console.log(error);
+    //     };
+    // };
+    
+    async function runTask() {
+        console.log("I'm going to work");
+
+        // Get current time and set to date variable
+        const currentTimeISO = new Date().toISOString();
+        const thirtyMinutesFromNow = new Date().getTime() + 1800000;
+        const thirtyMinutesISO = new Date(thirtyMinutesFromNow).toISOString();
+        console.log("\n Current time:", currentTimeISO, "\n Current time + 30:", thirtyMinutesISO);
+
+        const eventsToOpen = await sortAndFilterEvents(currentTimeISO, thirtyMinutesISO);
+        await openCheckins(eventsToOpen);
+
+        console.log("I finished working");
+    };
+
+    const scheduledTask = cron.schedule('*/15 * * * * *', () => {
+        runTask();
     });
 
-    return task;
+    return scheduledTask;
 };
