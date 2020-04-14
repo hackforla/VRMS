39a98ca752239ed507a94938a2545c879419e658
sarL3y <matt.tapper@gmail.com>
2020-04-14 12:41:54 -0700

    Rebase to development

--- client/src/App.js
+++ client/src/App.js
@@ -18,6 +18,7 @@ import Success from "./pages/Success";
 import HandleAuth from "./pages/HandleAuth";
 import EmailSent from "./pages/EmailSent";
 import Events from "./pages/Events";
+import AddNew from './pages/AddNew';
 
 import "./App.scss";
 
@@ -33,7 +34,8 @@ const routes = [
   { path: "/success", name: "success", Component: Success },
   { path: "/handleauth", name: "handleauth", Component: HandleAuth },
   { path: "/emailsent", name: "emailsent", Component: EmailSent },
-  { path: "/events", name: "events", Component: Events }
+  { path: "/events", name: "events", Component: Events },
+  { path: '/add/:item', name: 'addnew', Component: AddNew}
 ];
 
 const App = props => {
