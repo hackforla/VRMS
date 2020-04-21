d2efedf733430f82a457d6ddf50d60068338c861
Carlos <clockwerkz@gmail.com>
2020-04-21 15:58:49 -0700

    refactoring userprofile In prog

--- client/src/pages/UserProfile.js
+++ client/src/pages/UserProfile.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import '../sass/UserProfile.scss';
-
+import UserTable from '../components/presentational/UserTable';
 const UserProfile = (props) => {
 
     useEffect(() => {
@@ -15,26 +15,7 @@ const UserProfile = (props) => {
                     <h3 className="profile__title">My Profile</h3>
                 </div>
                 <div className="user-info">
-                    <table className="user-data">
-                        <tbody>
-                            <tr><th className="user-data__header">Name</th><td className="user-data__info user-data">John Smith</td></tr>
-                            <tr><th className="user-data__header">Email</th><td className="user-data__info">john.smith@test.com</td></tr>
-                            <tr><th className="user-data__header">Github</th><td className="user-data__info">www.github.com/j_smith</td></tr>
-                            <tr><th className="user-data__header">Slack</th><td className="user-data__info">www.slack.com/j_smith</td></tr>
-                            <tr><th className="user-data__header">Desired Roles</th><td className="user-data__info user-data__info--flex">
-                                <p className="user-data__selection"><span>UX</span><button className="user-data__delete">X</button></p>
-                                <p className="user-data__selection"><span>Front End</span><button className="user-data__delete">X</button></p>
-                            </td></tr>
-                            <tr><th className="user-data__header">My Hack Nights</th><td class="user-data__info user-data__info--flex">
-                                <p className="user-data__selection"><span>DTLA</span><button className="user-data__delete">X</button></p>
-                                <p className="user-data__selection"><span>Westside</span><button className="user-data__delete">X</button></p>
-                            </td></tr>
-                            <tr><th className="user-data__header">Availability</th><td class="user-data__info user-data__info--flex">
-                                <p className="user-data__selection"><span>Mon PM</span><button className="user-data__delete">X</button></p>
-                                <p className="user-data__selection"><span>Thurs PM</span><button className="user-data__delete">X</button></p>  
-                            </td></tr>
-                        </tbody>
-                    </table>
+                    <UserTable />
                 </div>
                 <div className="profile__header">
                     <h3 className="profile__subtitle">My Upcoming Events</h3>
