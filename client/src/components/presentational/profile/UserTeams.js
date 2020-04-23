cec78b177f32052743d32dc076602eb206731f43
Carlos <clockwerkz@gmail.com>
2020-04-23 09:55:04 -0700

    adds keys to trs to clear unique key warnings.

--- client/src/components/presentational/profile/UserTeams.js
+++ client/src/components/presentational/profile/UserTeams.js
@@ -12,8 +12,8 @@ const UserTeams = ({ context }) => {
                         <th className="user-data__header">Team</th>
                         <th className="user-data__header">Status</th>
                     </tr>
-                    { teams ? teams.map(team => 
-                        (<tr>
+                    { teams ? teams.map((team,index) => 
+                        (<tr key={index}>
                             <td className="user-data__info">{team.name}</td>
                         <td className={`user-data__info user-data__info--${team.status.toLowerCase()}`}>{team.status}</td>
                         </tr>)) 
