f1542d0c34e089a7183957c694085244b48fce6f
Patrick Gustafson <53308172+patrickgus@users.noreply.github.com>
2020-06-20 17:09:46 -0700

    create addMember function

--- client/src/components/dashboard/AddTeamMember.js
+++ client/src/components/dashboard/AddTeamMember.js
@@ -1,117 +1,140 @@
 import React, { useState } from "react";
-// import Firebase from '../../firebase';
 import "../../sass/AddTeamMember.scss";
 
 const AddTeamMember = (props) => {
-  const [isError, setIsError] = useState(false);
-  const [errorMessage, setErrorMessage] = useState("");
-  const [email, setEmail] = useState("");
+    const [isError, setIsError] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
+    const [email, setEmail] = useState("");
 
-  const handleInputChange = (e) => setEmail(e.currentTarget.value);
+    const handleInputChange = (e) => setEmail(e.currentTarget.value);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setIsError(false);
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setIsError(false);
 
-    if (email === "") {
-      setIsError(true);
-      setErrorMessage("Please don't leave the field blank.");
-    } else if (!email.includes("@") || !email.includes(".")) {
-      setIsError(true);
-      setErrorMessage("Please format the email address correctly.");
-    } else {
-      let hasEmail = await checkEmail(e);
-
-      if (hasEmail === false) {
+        if (email === "") {
         setIsError(true);
-        setErrorMessage("Email does not exist.");
-      } else {
-        console.log("Success.", email);
-        // Firebase.submitEmail(email)
-        //     .then(response => {
-        //         props.history.push('/emailsent');
-        //     })
-        //     .catch(error => {
-        //         console.log(error);
-        //     });
-      }
-    }
-  };
+        setErrorMessage("Please don't leave the field blank.");
+        } else if (!email.includes("@") || !email.includes(".")) {
+        setIsError(true);
+        setErrorMessage("Please format the email address correctly.");
+        } else {
+        let hasEmail = await checkEmail(e);
 
-  async function checkEmail(e) {
-    e.preventDefault();
-
-    try {
-      return await fetch("/api/checkuser", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ email }),
-      })
-        .then((res) => {
-          if (res.ok) {
-            return res.json();
-          }
-
-          throw new Error(res.statusText);
-        })
-        .then((response) => {
-          if (response === false) {
+        if (hasEmail === false) {
             setIsError(true);
-            setErrorMessage("Email not found.");
+            setErrorMessage("Email does not exist.");
+        } else {
+            await addMember(e);
+            console.log("Success. Team member added.", email);
+        }
+        }
+    };
 
-            return response;
-          } else {
-            return response.email;
-          }
+    async function addMember(e) {
+        // needs to add a team memember to a project
+        // project leader enters email into input and will add to projectteammember model
+        // need to specify which project if user is lead for multiple projects
+        e.preventDefault();
+
+        try {
+        return await fetch("/api/projectteammembers", {
+            method: "POST",
+            headers: {
+            "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ email }),
         })
-        .catch((err) => {
-          console.log(err);
-        });
-    } catch (error) {
-      console.log(error);
+            .then((res) => {
+            if (res.ok) {
+                console.log(res);
+                return res.json();
+            }
+
+            throw new Error(res.statusText);
+            })
+            .catch((err) => {
+            console.log(err);
+            });
+        } catch (error) {
+        console.log(error);
+        }
     }
-  }
 
-  return (
-    <div className="flex-container">
-      <div className="addmember-container">
-        <form
-          className="form-add-member"
-          autoComplete="off"
-          onSubmit={(e) => e.preventDefault()}
-        >
-          <div className="form-row">
-            <div className="form-input-text">
-              <label htmlFor="email">Add team member:</label>
-              <input
-                type="email"
-                name="email"
-                placeholder="Email Address"
-                value={email.toString()}
-                onChange={(e) => handleInputChange(e)}
-                aria-label="Email Address"
-                autoComplete="none"
-                required="required"
-              />
+    async function checkEmail(e) {
+        e.preventDefault();
+
+        try {
+        return await fetch("/api/checkuser", {
+            method: "POST",
+            headers: {
+            "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ email }),
+        })
+            .then((res) => {
+            if (res.ok) {
+                return res.json();
+            }
+
+            throw new Error(res.statusText);
+            })
+            .then((response) => {
+            if (response === false) {
+                setIsError(true);
+                setErrorMessage("Email not found.");
+
+                return response;
+            } else {
+                return response.email;
+            }
+            })
+            .catch((err) => {
+            console.log(err);
+            });
+        } catch (error) {
+        console.log(error);
+        }
+    }
+
+    return (
+        <div className="flex-container">
+            <div className="addmember-container">
+                <form
+                className="form-add-member"
+                autoComplete="off"
+                onSubmit={(e) => e.preventDefault()}
+                >
+                <div className="form-row">
+                    <div className="form-input-text">
+                    <label htmlFor="email">Add team member:</label>
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="Email Address"
+                        value={email.toString()}
+                        onChange={(e) => handleInputChange(e)}
+                        aria-label="Email Address"
+                        autoComplete="none"
+                        required="required"
+                    />
+                    </div>
+                </div>
+
+                <div className="form-input-button">
+                    <button
+                    onClick={(e) => handleSubmit(e)}
+                    className="addmember-button"
+                    >
+                    Add
+                    </button>
+                </div>
+                </form>
+
+                <div className="addmember-warning">{isError ? errorMessage : null}</div>
             </div>
-          </div>
-
-          <div className="form-input-button">
-            <button
-              onClick={(e) => handleSubmit(e)}
-              className="addmember-button"
-            >
-              Add
-            </button>
-          </div>
-        </form>
-
-        <div className="addmember-warning">{isError ? errorMessage : null}</div>
-      </div>
-    </div>
-  );
+        </div>
+    );
 };
 
 export default AddTeamMember;
