0e2822071aabde2ee8ff00947f1e5796903da639
Patrick Gustafson <53308172+patrickgus@users.noreply.github.com>
2020-07-27 13:16:39 -0700

    create new route that finds user in a project

--- routers/projectTeamMembers.router.js
+++ routers/projectTeamMembers.router.js
@@ -53,6 +53,23 @@ router.get("/project/:id/:userId", (req, res) => {
     });
 });
 
+router.get("/:id/:userId", (req, res) => {
+  ProjectTeamMember.find({
+		projectId: req.params.id,
+		userId: req.params.userId
+  })
+		.populate("userId")
+    .then((teamMembers) => {
+      res.status(200).json(teamMembers);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(400).json({
+        message: `/GET Internal server error:  ${err}`,
+      });
+    });
+});
+
 router.get("/projectowner/:id", (req, res) => {
   const id = req.params.id;
 
