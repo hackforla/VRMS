34c418f7cfdd91c5e9207df83f460883517311d7
Nick Beaird <nicklasbeaird@gmail.com>
2020-08-21 10:26:17 -0700

    Fix Answer model and add unit test

--- backend/models/answer.model.js
+++ backend/models/answer.model.js
@@ -1,34 +1,34 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
 
 const answerSchema = mongoose.Schema({
-    question: {                                          
-        questionId: { type: String },
-        htmlName: { type: String }
-    },
-    user: { 
-        userId: { type: String }
-    },
-    selectedAnswer: { type: String },
-    createdDate: { type: Date, default: Date.now }
+  question: {
+    questionId: { type: String },
+    htmlName: { type: String },
+  },
+  user: {
+    userId: { type: String },
+  },
+  selectedAnswer: { type: String },
+  createdDate: { type: Date, default: Date.now },
 });
 
-questionSchema.methods.serialize = function() {
-    return {
-        id: this._id,
-        question: {                                          
-            questionID: this.question.questionId,
-            htmlName: this.question.htmlName 
-        },
-        user: {
-            userId: this.user.userId
-        },
-        selectedAnswer: this.selectedAnswer,
-        createdDate: this.createdDate
-    };
+questionSchema.methods.serialize = function () {
+  return {
+    id: this._id,
+    question: {
+      questionID: this.question.questionId,
+      htmlName: this.question.htmlName,
+    },
+    user: {
+      userId: this.user.userId,
+    },
+    selectedAnswer: this.selectedAnswer,
+    createdDate: this.createdDate,
+  };
 };
 
-const Answer = mongoose.model('Answer', answerSchema);
+const Answer = mongoose.model("Answer", answerSchema);
 
-module.exports = { Answer };
+module.exports = Answer;
