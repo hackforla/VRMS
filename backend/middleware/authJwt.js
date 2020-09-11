72fb8568156c6ba2fefe7a4e83178891bbca92a3
Nick Beaird <nicklasbeaird@gmail.com>
2020-09-11 13:25:36 -0700

    Verify token on the backend

--- backend/middleware/authJwt.js
+++ backend/middleware/authJwt.js
@@ -2,14 +2,25 @@ const jwt = require("jsonwebtoken");
 const CONFIG = require("../config/auth.config.js");
 
 function verifyToken(req, res, next) {
-  let token = req.headers["x-access-token"];
+  let token = req.headers["x-access-token"] || req.headers["authorization"];
 
   if (!token) {
-    return res.status(403).send({ message: "No token provided!" });
+    return res.status(403).send({ message: "Auth token is not supplied" });
+  }
+  if (token.startsWith("Bearer ")) {
+    // Remove Bearer from string
+    token = token.slice(7, token.length);
   }
 
   jwt.verify(token, CONFIG.SECRET, (err, decoded) => {
     if (err) {
+      return res.status(401).send({ message: err });
+    }
+    res.cookie("token", token, { httpOnly: true });
+    req.userId = decoded.id;
+    next();
+  });
+}
 
 function verifyCookie(req, res, next) {
   jwt.verify(req.cookies.token, CONFIG.SECRET, (err, decoded) => {
