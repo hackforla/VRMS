4609832e8f3e85915194706a454de58b188d31d2
Nick Beaird <nicklasbeaird@gmail.com>
2020-09-12 17:52:09 -0700

    eslint top level client files

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 // import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>, 
-    document.getElementById('root')
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,13 +1,16 @@
 // setupProxy.js - Dynamically setup a proxy from the frontend to the backend. This file
 // does not need to be imported. https://create-react-app.dev/docs/adding-custom-environment-variables/
 
-const { createProxyMiddleware } = require("http-proxy-middleware");
-module.exports = function (app) {
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+function setupProxy(app) {
   app.use(
-    "/api",
+    '/api',
     createProxyMiddleware({
       target: process.env.REACT_APP_PROXY,
       changeOrigin: true,
     })
   );
-};
+}
+
+module.exports = setupProxy;
