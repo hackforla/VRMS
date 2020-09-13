250c851579fd68f8a2dd7ce4b654f58d0df46e5c
Nick Beaird <nicklasbeaird@gmail.com>
2020-09-12 22:00:35 -0700

    Fix base eslint Success issues

--- client/src/pages/Success.js
+++ client/src/pages/Success.js
@@ -1,47 +1,27 @@
-import React, { useEffect } from "react";
-// import { Link } from 'react-router-dom';
+import React from 'react';
 
 import "../sass/MagicLink.scss";
 
 const Success = (props) => {
-  console.log("PROPS", props);
-  // const [isLoading, setIsLoading] = useState(false);
-  // const [event, setEvent] = useState([]);
-  // const [isError, setIsError] = useState(null);
-  // const [magicLink, setMagicLink] = useState('https://localhost:3000/api/happyURLfuntime');
-
-  // async function fetchData() {
-  //     try {
-  //         const res = await fetch(`http://localhost:4000/api/events/${props.match.params.id}`);
-  //         const resJson = await res.json();
-  //         setEvent(resJson);
-  //     } catch(error) {
-  //         setIsError(error);
-  //         alert(error);
-  //     }
-  // }
-
-  // useEffect(() => {
-  //     // fetchData();
-  //     // let timer = setTimeout(() => {
-  //     //     props.history.push('/');
-  //     // }, 6000);
-
-  //     // return () => clearTimeout(timer);
-  // }, []);
+  console.log('PROPS', props);
 
   return (
     <div className="flex-container">
       <div className="new">
-        {/* <div class="rotated-success"></div> */}
         <div className="new-headers">
           <h3 className="last-row">Success!</h3>
           <h4 className="last-row">Soon, you'll be able to: </h4>
         </div>
         <div className="future-list">
-          <p>👉 View your detailed, personalized journey...</p>
-          <p>👉 Get matched with projects that need you...</p>
-          <p>👉 Manage your own project!</p>
+          <p>  
+            <span role="img" aria-label="step1">👉</span> View your detailed, personalized journey...
+          </p>
+          <p>
+            <span role="img" aria-label="step2">👉</span> Get matched with projects that need you...
+          </p>
+          <p>
+            <span role="img" aria-label="step3">👉</span> Manage your own project!
+          </p>
         </div>
 
         <div className="success-info">
