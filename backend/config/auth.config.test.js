9e4491ee9efdc169d2c80a701cb885a2e157a21e
Nick Beaird <nicklasbeaird@gmail.com>
2020-09-16 16:51:49 -0700

    Fix eslint errors in auth

--- backend/config/auth.config.test.js
+++ backend/config/auth.config.test.js
@@ -1,5 +1,3 @@
-const { expectCt } = require('helmet');
-
 test('Environment variables are working as expected', () => {
   const backendUrl = process.env.REACT_APP_PROXY;
   expect(backendUrl === `http://localhost:${process.env.BACKEND_PORT}`);
