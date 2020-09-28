529a6bd5b0e4497bea2a55af79e5aaba9a91fb6c
Nick Beaird <nicklasbeaird@gmail.com>
2020-09-27 20:48:18 -0700

    Revert "Run prettier on admin page"
    This reverts commit 15203beb8288ad4a8ea15f6c47d6474ace298535.

--- client/src/pages/AdminDashboard.js
+++ client/src/pages/AdminDashboard.js
@@ -1,17 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import { Redirect } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Redirect } from "react-router-dom";
 
-import useAuth from '../hooks/useAuth';
+import useAuth from "../hooks/useAuth";
 
-import '../sass/Dashboard.scss';
-import UpcomingEvent from '../components/presentational/upcomingEvent';
-import EventOverview from '../components/presentational/eventOverview';
-import DonutChartContainer from '../components/presentational/donutChartContainer';
-import Loading from '../components/presentational/donutChartLoading';
+import "../sass/Dashboard.scss";
+import UpcomingEvent from "../components/presentational/upcomingEvent";
+import EventOverview from "../components/presentational/eventOverview";
+import DonutChartContainer from "../components/presentational/donutChartContainer";
+import Loading from "../components/presentational/donutChartLoading";
 
 const AdminDashboard = (props) => {
   const auth = useAuth();
-  const defaultChartType = 'All Events';
+  const defaultChartType = "All Events";
   let uniqueEventTypes = new Set();
   let hackNightUniqueLocations = new Set();
 
@@ -24,31 +24,14 @@ const AdminDashboard = (props) => {
   const [chartTypes, setChartTypes] = useState(null);
 
   // Volunteers SignedIn By Event Type
-  const [
-    totalVolunteersByEventType,
-    setVolunteersSignedInByEventType,
-  ] = useState({});
-  const [
-    totalVolunteerHoursByEventType,
-    setVolunteeredHoursByEventType,
-  ] = useState({});
-  const [totalVolunteerAvgHoursByEventType, setAvgHoursByEventType] = useState(
-    {}
-  );
+  const [totalVolunteersByEventType, setVolunteersSignedInByEventType] = useState({});
+  const [totalVolunteerHoursByEventType, setVolunteeredHoursByEventType] = useState({});
+  const [totalVolunteerAvgHoursByEventType, setAvgHoursByEventType] = useState({});
 
   // Volunteers SignedIn By Hacknight Property
-  const [
-    totalVolunteersByHacknightProp,
-    setVolunteersSignedInByHacknightProp,
-  ] = useState({});
-  const [
-    totalVolunteerHoursByHacknightProp,
-    setVolunteeredHoursByHacknightProp,
-  ] = useState({});
-  const [
-    totalVolunteerAvgHoursByHacknightProp,
-    setAvgHoursByHacknightProp,
-  ] = useState({});
+  const [totalVolunteersByHacknightProp, setVolunteersSignedInByHacknightProp] = useState({});
+  const [totalVolunteerHoursByHacknightProp, setVolunteeredHoursByHacknightProp] = useState({});
+  const [totalVolunteerAvgHoursByHacknightProp, setAvgHoursByHacknightProp] = useState({});
 
   // Volunteers To Chart
   const [totalVolunteers, setVolunteersToChart] = useState({});
@@ -60,9 +43,9 @@ const AdminDashboard = (props) => {
   async function getAndSetData() {
     try {
       setIsLoading(true);
-      const checkIns = await fetch('/api/checkins');
+      const checkIns = await fetch("/api/checkins");
       const checkInsJson = await checkIns.json();
-      const events = await fetch('/api/events');
+      const events = await fetch("/api/events");
       const eventsJson = await events.json();
 
       processData(eventsJson, checkInsJson);
@@ -73,7 +56,7 @@ const AdminDashboard = (props) => {
     }
   }
 
-  function processData(allEvents, allCheckIns) {
+  function processData(allEvents, allCheckIns){
     let processedEvents = processEvents(allEvents);
     let usersByEvent = collectUsersByEvent(allCheckIns);
     prepareDataForCharts(processedEvents, usersByEvent);
@@ -82,14 +65,14 @@ const AdminDashboard = (props) => {
   function processEvents(allEvents) {
     let events = new Map();
 
-    for (let event of allEvents) {
+    for (let event of allEvents){
       // Process legacy data with undefined 'hours' property because initially an event length was 3 hours
-      if (!event.hours) {
+      if(!event.hours){
         event.hours = 3;
       }
 
       // Define unique event types and process events without 'eventType' property
-      if (event.eventType) {
+      if(event.eventType){
         processEventTypes(event, 'eventType', uniqueEventTypes);
       } else {
         // Find events without 'eventType' property (30 events) and assign it
@@ -97,7 +80,7 @@ const AdminDashboard = (props) => {
       }
 
       // Extract events with 'hacknight' property & find unique locations in it
-      if (event.hacknight) {
+      if(event.hacknight){
         processEventTypes(event, 'hacknight', hackNightUniqueLocations);
       }
       events.set(event._id, event);
@@ -106,83 +89,58 @@ const AdminDashboard = (props) => {
     return events;
   }
 
-  function processEventTypes(event, propName, uniqueTypes) {
+  function processEventTypes(event, propName, uniqueTypes){
     const capitalize = (str, lower = false) =>
-      (lower ? str.toLowerCase() : str).replace(
-        /(?:^|\s|["'([{])+\S/g,
-        (match) => match.toUpperCase()
-      );
+        (lower ? str.toLowerCase() : str).replace(/(?:^|\s|["'([{])+\S/g, match => match.toUpperCase());
     let type = capitalize(event[propName], true);
     event[propName] = type;
     uniqueTypes.add(type);
   }
 
-  function createChartTypes() {
+  function createChartTypes(){
     let chartTypes = {
-      'All Events': '',
-      'Hacknight Only': '',
+      "All Events": "",
+      "Hacknight Only": ""
     };
     setChartTypes(chartTypes);
   }
 
-  function collectUsersByEvent(allCheckIns) {
+  function collectUsersByEvent(allCheckIns){
     let eventCollection = new Map();
-    for (let checkIn of allCheckIns) {
-      if (eventCollection.has(checkIn.eventId)) {
+    for(let checkIn of allCheckIns){
+      if(eventCollection.has(checkIn.eventId)){
         eventCollection.get(checkIn.eventId).push(checkIn);
-      } else {
+      } else{
         eventCollection.set(checkIn.eventId, [checkIn]);
       }
     }
     return eventCollection;
   }
 
-  function prepareDataForCharts(events, users) {
+  function prepareDataForCharts(events, users){
     // Data for 1 chart 'total volunteers'
-    let totalVolunteersByEventType = extractVolunteersSignedInByProperty(
-      events,
-      users,
-      uniqueEventTypes,
-      'eventType'
-    );
+    let totalVolunteersByEventType = extractVolunteersSignedInByProperty(events, users, uniqueEventTypes, 'eventType');
     setVolunteersSignedInByEventType(totalVolunteersByEventType);
-    let totalVolunteersByHacknightProp = extractVolunteersSignedInByProperty(
-      events,
-      users,
-      hackNightUniqueLocations,
-      'hacknight'
-    );
+    let totalVolunteersByHacknightProp = extractVolunteersSignedInByProperty(events, users, hackNightUniqueLocations, 'hacknight');
     setVolunteersSignedInByHacknightProp(totalVolunteersByHacknightProp);
 
     // Data for 2 chart 'total hours'
-    let totalVolunteerHoursByEventType = findTotalVolunteerHours(
-      events,
-      users,
-      uniqueEventTypes,
-      'eventType'
-    );
+    let totalVolunteerHoursByEventType = findTotalVolunteerHours(events, users, uniqueEventTypes, 'eventType');
     setVolunteeredHoursByEventType(totalVolunteerHoursByEventType);
-    let totalVolunteerHoursByHacknightProp = findTotalVolunteerHours(
-      events,
-      users,
-      hackNightUniqueLocations,
-      'hacknight'
-    );
+    let totalVolunteerHoursByHacknightProp = findTotalVolunteerHours(events, users, hackNightUniqueLocations, 'hacknight');
     setVolunteeredHoursByHacknightProp(totalVolunteerHoursByHacknightProp);
 
     //  Data for 3 chart 'total average hours'
     let totalVolunteerAvgHoursByEventType = findAverageVolunteerHours(
-      totalVolunteersByEventType,
-      totalVolunteerHoursByEventType,
-      uniqueEventTypes
-    );
+        totalVolunteersByEventType,
+        totalVolunteerHoursByEventType,
+        uniqueEventTypes);
     setAvgHoursByEventType(totalVolunteerAvgHoursByEventType);
 
     let totalVolunteerAvgHoursByHacknightProp = findAverageVolunteerHours(
-      totalVolunteersByHacknightProp,
-      totalVolunteerHoursByHacknightProp,
-      hackNightUniqueLocations
-    );
+        totalVolunteersByHacknightProp,
+        totalVolunteerHoursByHacknightProp,
+        hackNightUniqueLocations);
     setAvgHoursByHacknightProp(totalVolunteerAvgHoursByHacknightProp);
 
     // Display data by default for "All" chart type
@@ -191,26 +149,18 @@ const AdminDashboard = (props) => {
     setAvgHoursToChart(totalVolunteerAvgHoursByEventType);
   }
 
-  function extractVolunteersSignedInByProperty(
-    events,
-    users,
-    uniqueTypes,
-    propName
-  ) {
+  function extractVolunteersSignedInByProperty(events, users, uniqueTypes, propName){
     let result = {};
     let type;
 
-    uniqueTypes.forEach((el) => (result[el] = parseInt('0')));
+    uniqueTypes.forEach(el => result[el] = parseInt('0'));
     for (let eventId of users.keys()) {
-      if (propName === 'eventType') {
+      if(propName === 'eventType'){
         type = events.get(eventId).eventType;
         result[type] = users.get(eventId).length + result[type];
       }
 
-      if (
-        propName === 'hacknight' &&
-        typeof events.get(eventId).hacknight !== 'undefined'
-      ) {
+      if(propName === 'hacknight' && typeof events.get(eventId).hacknight !== 'undefined'){
         type = events.get(eventId).hacknight;
         result[type] = users.get(eventId).length + result[type];
       }
@@ -218,58 +168,46 @@ const AdminDashboard = (props) => {
     return result;
   }
 
-  function findTotalVolunteerHours(events, users, uniqueTypes, propName) {
+  function findTotalVolunteerHours(events, users, uniqueTypes, propName){
     let result = {};
     let type;
-    uniqueTypes.forEach((el) => (result[el] = parseInt('0')));
+    uniqueTypes.forEach(el => result[el] = parseInt('0'));
 
     for (let eventId of users.keys()) {
-      if (propName === 'eventType') {
+      if(propName === 'eventType'){
         type = events.get(eventId).eventType;
-        result[type] =
-          result[type] + events.get(eventId).hours * users.get(eventId).length;
+        result[type] = result[type] + (events.get(eventId).hours * users.get(eventId).length);
       }
 
-      if (
-        propName === 'hacknight' &&
-        typeof events.get(eventId).hacknight !== 'undefined'
-      ) {
+      if (propName === 'hacknight' && typeof events.get(eventId).hacknight !== 'undefined'){
         type = events.get(eventId).hacknight;
-        result[type] =
-          result[type] + events.get(eventId).hours * users.get(eventId).length;
+        result[type] = result[type] + (events.get(eventId).hours * users.get(eventId).length);
       }
     }
     return result;
   }
 
-  function findAverageVolunteerHours(
-    totalVolunteers,
-    totalVolunteerHours,
-    uniqueTypes
-  ) {
+  function findAverageVolunteerHours(totalVolunteers, totalVolunteerHours, uniqueTypes){
     let result = {};
-    uniqueTypes.forEach((el) => (result[el] = parseInt('0')));
+    uniqueTypes.forEach(el => result[el] = parseInt('0'));
 
-    for (let eventType of uniqueTypes.keys()) {
+    for(let eventType of uniqueTypes.keys()){
       let hours = totalVolunteerHours[eventType];
       let volunteers = totalVolunteers[eventType];
-      let averageHours = hours / volunteers;
-      if (!Number.isInteger(averageHours))
-        averageHours = +averageHours.toFixed(2);
-      volunteers > 0
-        ? (result[eventType] = averageHours)
-        : (result[eventType] = 0);
+      let averageHours = (hours / volunteers);
+      if(!Number.isInteger(averageHours)) averageHours = +averageHours.toFixed(2);
+      volunteers > 0 ? result[eventType] = averageHours : result[eventType] = 0;
     }
     return result;
   }
 
-  function setDonutCharts(valueFromSelect) {
+  function setDonutCharts(valueFromSelect){
     // Display data depending on value from select
-    if (valueFromSelect === defaultChartType) {
+    if(valueFromSelect === defaultChartType){
       setVolunteersToChart(totalVolunteersByEventType);
       setVolunteeredHoursToChart(totalVolunteerHoursByEventType);
       setAvgHoursToChart(totalVolunteerAvgHoursByEventType);
-    } else if (valueFromSelect === 'Hacknight Only') {
+    } else if (valueFromSelect === "Hacknight Only"){
       setVolunteersToChart(totalVolunteersByHacknightProp);
       setVolunteeredHoursToChart(totalVolunteerHoursByHacknightProp);
       setAvgHoursToChart(totalVolunteerAvgHoursByHacknightProp);
@@ -281,10 +219,10 @@ const AdminDashboard = (props) => {
 
     try {
       setIsLoading(true);
-      const users = await fetch('/api/users', {
+      const users = await fetch("/api/users", {
         headers: {
-          'Content-Type': 'application/json',
-          'x-customrequired-header': headerToSend,
+          "Content-Type": "application/json",
+          "x-customrequired-header": headerToSend,
         },
       });
       const usersJson = await users.json();
@@ -303,7 +241,7 @@ const AdminDashboard = (props) => {
     try {
       setIsLoading(true);
 
-      const events = await fetch('/api/events');
+      const events = await fetch("/api/events");
       const eventsJson = await events.json();
 
       const dates = eventsJson.map((event) => {
@@ -333,9 +271,9 @@ const AdminDashboard = (props) => {
 
     try {
       await fetch(`/api/events/${nextEventId}`, {
-        method: 'PATCH',
+        method: "PATCH",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
       }).then((response) => {
         if (response.ok) {
@@ -359,64 +297,66 @@ const AdminDashboard = (props) => {
     getUsers();
   }, []);
 
-  return auth && auth.user ? (
-    <div className="flex-container">
-      <div className="dashboard">
-        <div className="dashboard-header">
-          <p className="dashboard-header-text-small">
-            You have an event coming up:
-          </p>
+  return (
+    auth && auth.user ? (
+      <div className="flex-container">
+        <div className="dashboard">
+          <div className="dashboard-header">
+            <p className="dashboard-header-text-small">
+              You have an event coming up:
+            </p>
+          </div>
+
+          {isLoading ? (
+              <Loading />
+          ) : (
+              <UpcomingEvent
+                  isCheckInReady={isCheckInReady}
+                  nextEvent={nextEvent}
+                  setCheckInReady={setCheckInReady}
+              />
+          )}
+
+          {isLoading ? (
+              <Loading />
+          ) : (
+              <EventOverview
+                  handleChartTypeChange={handleChartTypeChange}
+                  chartTypes={chartTypes}
+              />
+          )}
+
+          {isLoading ? (
+              <Loading />
+          ) : (
+              <DonutChartContainer
+                  chartName={"Total Volunteers"}
+                  data={totalVolunteers}
+              />
+          )}
+
+          {isLoading ? (
+              <Loading />
+          ) : (
+              <DonutChartContainer
+                  chartName={"Total Volunteer Hours"}
+                  data={totalVolunteerHours}
+              />
+          )}
+
+          {isLoading ? (
+              <Loading />
+          ) : (
+              <DonutChartContainer
+                  chartName={"Average Hours Per Volunteer"}
+                  data={totalVolunteerAvgHours}
+              />
+          )}
         </div>
-
-        {isLoading ? (
-          <Loading />
-        ) : (
-          <UpcomingEvent
-            isCheckInReady={isCheckInReady}
-            nextEvent={nextEvent}
-            setCheckInReady={setCheckInReady}
-          />
-        )}
-
-        {isLoading ? (
-          <Loading />
-        ) : (
-          <EventOverview
-            handleChartTypeChange={handleChartTypeChange}
-            chartTypes={chartTypes}
-          />
-        )}
-
-        {isLoading ? (
-          <Loading />
-        ) : (
-          <DonutChartContainer
-            chartName={'Total Volunteers'}
-            data={totalVolunteers}
-          />
-        )}
-
-        {isLoading ? (
-          <Loading />
-        ) : (
-          <DonutChartContainer
-            chartName={'Total Volunteer Hours'}
-            data={totalVolunteerHours}
-          />
-        )}
-
-        {isLoading ? (
-          <Loading />
-        ) : (
-          <DonutChartContainer
-            chartName={'Average Hours Per Volunteer'}
-            data={totalVolunteerAvgHours}
-          />
-        )}
       </div>
-    </div>
-  ) : (
-    <Redirect to="/login" />
+    ) : (
+        <Redirect to="/login" />
+    )
   );
 };
 
