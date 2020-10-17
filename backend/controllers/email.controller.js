72625c7219ea1387cc0475631ea8849bbf2189fe
Nick Beaird <nicklasbeaird@gmail.com>
2020-10-16 17:49:58 -0700

    Refactor email controller

--- backend/controllers/email.controller.js
+++ backend/controllers/email.controller.js
@@ -1,5 +1,6 @@
 const nodemailer = require('nodemailer');
 const { google } = require('googleapis');
+const { resolveConfig } = require('prettier');
 
 const { OAuth2 } = google.auth;
 
@@ -8,7 +9,8 @@ const SECRET_ID = process.env.GMAIL_SECRET_ID;
 const REFRESH_TOKEN = process.env.GMAIL_REFRESH_TOKEN;
 const EMAIL_ACCOUNT = process.env.GMAIL_EMAIL;
 
-async function mailServer(email, token) {
+
+const emailCientToken = async () => {
   const oauth2Client = new OAuth2(
     CLIENT_ID, // ClientID
     SECRET_ID, // Client Secret
@@ -18,32 +20,40 @@ async function mailServer(email, token) {
   oauth2Client.setCredentials({
     refresh_token: REFRESH_TOKEN,
   });
-  const accessToken = oauth2Client.getAccessToken();
+  accessToken = oauth2Client.getAccessToken();
+  return accessToken;
+};
 
-  let smtpTransport;
-  if (process.env.NODE_ENV === 'test') {
-    // Send mail to Mailhog Docker container
-    smtpTransport = nodemailer.createTransport({
-      host: '127.0.0.1',
-      port: 1025,
-      auth: {
-        user: 'user',
-        pass: 'password',
-      },
-    });
-  } else {
-    smtpTransport = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        type: 'OAuth2',
-        user: EMAIL_ACCOUNT,
-        clientId: CLIENT_ID,
-        clientSecret: SECRET_ID,
-        refreshToken: REFRESH_TOKEN,
-        accessToken,
-      },
-    });
-  }
+const createDockerSMTPSTransport = async () => {
+  // Send mail to Mailhog Docker container
+  const smtpTransport = nodemailer.createTransport({
+    host: 'mailhog',
+    port: 1025,
+    auth: {
+      user: 'user',
+      pass: 'password',
+    },
+  });
+  return smtpTransport;
+};
+
+const createProdSMTPTransport = async () => {
+  const accessToken = await emailCientToken();
+  const smtpTransport = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      type: 'OAuth2',
+      user: EMAIL_ACCOUNT,
+      clientId: CLIENT_ID,
+      clientSecret: SECRET_ID,
+      refreshToken: REFRESH_TOKEN,
+      accessToken,
+    },
+  });
+  return smtpTransport;
+};
+
+async function sendMail(smtpTransport, email, token) {
   const encodedToken = encodeURIComponent(token);
   const emailLink = `https://tinyurl.com/nyqxd/handleauth?token=${encodedToken}&signIn=true`;
   const encodedUri = encodeURI(emailLink);
@@ -57,27 +67,29 @@ async function mailServer(email, token) {
     text: `Magic link: ${emailLink}`,
   };
 
-  const localhostEmail = async () => {
-    await smtpTransport.sendMail(mailOptions, (error, response) => {
-      console.log("email sent");
-      smtpTransport.close();
-    });
-  }
+  let info = await smtpTransport.sendMail(mailOptions);
+  // console.log('Message sent: %s', info.messageId);
 
-  const prodEmail = async () => {
-    await smtpTransport.sendMail(mailOptions, (error, response) => {
-      error ? console.log(error) : console.log(response);
-      smtpTransport.close();
-    });
-  }
+  // await smtpTransport.close();
+};
 
-  if (process.env.NODE_ENV === 'test') {
-    localhostEmail();
+async function mailServer(email, token) {
+  let smtpTransport;
+  if (process.env.NODE_ENV === 'production') {
+    smtpTransport = await createProdSMTPTransport();
   } else {
-    prodEmail();
+    smtpTransport = await createDockerSMTPSTransport();
   }
+
+  sendMail(smtpTransport, email, token).catch(console.error);
 }
 
-exports.sendUserEmailSigninLink = async (email, token) => {
+const sendUserEmailSigninLink = async (email, token) => {
   await mailServer(email, token);
 };
+
+const emailController = {
+  sendUserEmailSigninLink,
+};
+module.exports = emailController;
+
