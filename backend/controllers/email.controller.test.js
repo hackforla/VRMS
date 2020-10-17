a83fe2c896d9046e4d20d5a5a0cab37c07ac17a1
Nick Beaird <nicklasbeaird@gmail.com>
2020-10-16 19:44:28 -0700

    Remove unnecessary auth

--- backend/controllers/email.controller.test.js
+++ backend/controllers/email.controller.test.js
@@ -1,6 +1,8 @@
 const emailController = require('./email.controller');
 
 test('Can import the email controller', async () => {
-  console.log(emailController);
   expect(emailController).not.toBeUndefined();
 });
+
+
+
