4f7ee989eabfd5d2124fb97923b3300f9777e41e
Nick Beaird <nicklasbeaird@gmail.com>
2020-10-16 18:48:21 -0700

    Fix integration tests

--- backend/controllers/email.controller.test.js
+++ backend/controllers/email.controller.test.js
@@ -1,6 +1,5 @@
 const emailController = require('./email.controller');
 
 test('Can import the email controller', async () => {
-  console.log(emailController);
   expect(emailController).not.toBeUndefined();
 });
