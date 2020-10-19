7b1a7608a3c3a3939e613ed64f6b8436b568c1a2
Susan Ye <susanye3@gmail.com>
2020-10-19 15:23:04 -0700

    added onboard bot code to slack router

--- backend/routers/slack.router.js
+++ backend/routers/slack.router.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const express = require("express");
 const router = express.Router();
 const { App } = require("@slack/bolt");
 const cron = require("node-cron");
 const Event = require("../models/event.model");
 const Project = require("../models/project.model");
+const User = require("../models/user.model");
+const CheckIn = require("../models/checkIn.model");
+
 
 //https://api.slack.com/web
 
@@ -23,6 +27,7 @@ if (process.env.NODE_ENV !== "test") {
   })();
 }
 
+//================ SLACKBOT FOR MEETING/EVENT REMINDERS ===============
 //Finds Id number of channel
 router.get("/findId", (req, res) => {
   publishMessage();
@@ -111,4 +116,99 @@ async function findProject(req, res) {
       });
     });
 }
+
+
+
+//============= SLACKBOT FOR SENDING NEW USERS MESSAGES AND REMINDERS ==============
+
+//----------------- FETCH LIST OF NEW USERS (LESS THAN 30 DAYS) --------------------
+//function to return list of users who are less than 30days new to HFLA
+async function findNewUsers(req, res) {
+  let newUserList = [];
+  User.find({})
+    .then((user) => {
+      user.forEach((cur) => {
+        let accountAge = checkStartDate(cur.createdDate);
+        if (accountAge <= 30) {
+          newUserList.push(cur);
+        }
+      });
+      //res.json(User);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(500).json({
+        message: `Error: ${err}`,
+      });
+    });
+    return newUserList;
+}
+console.log(findNewUsers());
+
+//function to check how long a user has been with the org (according to the date they created their VRMS account)
+function checkStartDate(userCreatedDate) {
+  let userStartDate = userCreatedDate;
+  let userStartparsed = Date.parse(userStartDate);
+  
+  let dateNow = new Date();
+  let diff = dateNow - userStartparsed;
+  let diffDays = diff/1000/60/60/24;
+  
+  return diffDays;
+}
+
+
+// Check status of user - using their userId and looking through the checkIn model for their userId
+async function userActivity(userList) {
+  try {
+    let newUsersList = findNewUsers()
+    let toMessageList = [];
+    
+    return toMessageList;
+  }
+  catch (error) {
+    console.error(error);
+  }
+}
+//userActivity(); 
+
+
+//------------MESSAGE USER FROM MARKDOWN BASED ON STATUS(ACTIVE/INACTIVE) -------------
+//based on user ID, will send user a DM from the bot with a message
+async function botDmChat() {
+  try {
+    
+    //using conversations.open to find user's conversation object
+    //TO DO: MAKE USER_ID DYNAMIC BASED ON DATABASE INFO/FUNCTION TO QUERY WHO TO MESSAGE
+    const userConversations = await app.client.conversations.open({
+      token: process.env.SLACK_BOT_TOKEN,
+      users: process.env.USER_ID
+    });
+    
+    //extracting user's DM id from conversation object
+    let userDmId = userConversations.channel.id;
+    
+    //grabbing user's name from user object api
+      let userObj = await app.client.users.info({
+      token: process.env.SLACK_BOT_TOKEN,
+      users: process.env.USER_ID
+    });
+    let userName = userObj.users[0].real_name;
+    let fileName = ''
+    let textData = fs.readFileSync(fileName,'utf8');
+
+    //using user userDmId to send message to user
+    const sendUserMsgObj = await app.client.chat.postMessage({
+      token: process.env.SLACK_BOT_TOKEN,
+      channel: userDmId,
+      mrkdown: true,
+      text: textData
+    });
+  }
+  catch (error) {
+    console.error(error);
+  }
+}
+
+
 module.exports = router;
