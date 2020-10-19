b9e2903c9d84a159a0bbd63d2c19b6efe8feac5e
OlgaBelogurova <obelogurova2@gmail.com>
2020-10-19 14:37:03 -0700

    Create buttons for the main section

--- client-mvp-04/src/components/main/index.js
+++ client-mvp-04/src/components/main/index.js
@@ -6,6 +6,9 @@ const Main = () => {
     <main className="app-main">
       <div className="app-name">VRMS</div>
       <h1 className="app-title">Volunteer Relationship Management System</h1>
+      <button className="main-button">Sign in</button>
+      <span className="main-text">or</span>
+      <div className="main-link">create account</div>
     </main>
   );
 };
