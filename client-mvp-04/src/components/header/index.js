0793bac3aff81c70227581cd31e11854fc09d896
OlgaBelogurova <obelogurova2@gmail.com>
2020-10-20 13:11:11 -0700

    Add navigation for Header

--- client-mvp-04/src/components/header/index.js
+++ client-mvp-04/src/components/header/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './index.scss';
 import logo from '../../assets/images/logo.svg';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header className="app-header">
-      <img src={logo} className="app-logo" alt="logo" />
+      <Link key={'header-home'} to={'/'} className="redirect-link">
+        <img src={logo} className="app-logo" alt="logo" />
+      </Link>
     </header>
   );
 };
