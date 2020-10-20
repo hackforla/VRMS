0f1328420bd04e7342bee112f6f86243e49bde37
OlgaBelogurova <obelogurova2@gmail.com>
2020-10-20 13:03:40 -0700

    Create the dummy page, add styles

---
+++ client-mvp-04/src/components/dummy/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.scss';
+
+const Dummy = () => {
+  return (
+    <section className="dummy-container">
+      <h2 className="dummy-content">
+        Sorry, this functionality is not implemented yet.
+      </h2>
+    </section>
+  );
+};
+
+export default Dummy;
