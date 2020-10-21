dce7064554b322e97e306c948a2b805a7254338c
Nick Beaird <nicklasbeaird@gmail.com>
2020-10-21 14:58:35 -0700

    Add ProjectController to JS imports

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -1,9 +1,11 @@
 const emailController = require('./email.controller');
 const EventController = require('./event.controller');
 const userController = require('./user.controller');
+const ProjectController = require('./project.controller');
 
 module.exports = {
   emailController,
   EventController,
   userController,
+  ProjectController,
 };
