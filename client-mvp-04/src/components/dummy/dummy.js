e3b16b00e8b170fe31d521ff79a3c65c02d43ce8
OlgaBelogurova <obelogurova2@gmail.com>
2020-10-23 23:11:10 -0700

    Add unit test to Dummy component

--- client-mvp-04/src/components/dummy/dummy.js
+++ client-mvp-04/src/components/dummy/dummy.js
@@ -3,7 +3,7 @@ import './dummy.scss';
 
 const Dummy = () => {
   return (
-    <section className="dummy-container">
+    <section data-testid="dummy" className="dummy-container">
       <h2 className="dummy-content">
         Sorry, this functionality is not implemented yet.
       </h2>
