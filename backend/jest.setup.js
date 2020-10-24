bc946b46f4eee30cfc11d795fe85e23873308848
Nick Beaird <nicklasbeaird@gmail.com>
2020-10-24 13:58:43 -0700

    Add comment

--- backend/jest.setup.js
+++ backend/jest.setup.js
@@ -1,5 +1,7 @@
 jest.setTimeout(30000)
 
+// TODO: Refactor worker routes. These are setup to run cron jobs every time the app
+// is instantiated. These break any integration tests. 
 jest.mock('./workers/openCheckins');
 jest.mock('./workers/closeCheckins');
 jest.mock('./workers/createRecurringEvents');
