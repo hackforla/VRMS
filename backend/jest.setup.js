37420f1186a2c900eea49d975fdfc15ee2e7cd79
Nick Beaird <nicklasbeaird@gmail.com>
2020-10-24 13:10:02 -0700

    Enable workers with test mocks

--- backend/jest.setup.js
+++ backend/jest.setup.js
@@ -1,1 +1,6 @@
 jest.setTimeout(30000)
+
+jest.mock('./workers/openCheckins');
+jest.mock('./workers/closeCheckins');
+jest.mock('./workers/createRecurringEvents');
+jest.mock('./workers/slackbot');
