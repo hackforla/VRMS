a62ffea8dae1cb6cc6fe0b5b2a234c31a9b664a9
OlgaBelogurova <obelogurova2@gmail.com>
2020-10-26 15:45:45 -0700

    Add test to Footer 'Should navigate to dummy page after clicking on the link'

--- client-mvp-04/src/components/common/footer/footer.test.js
+++ client-mvp-04/src/components/common/footer/footer.test.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import { render, screen, cleanup } from '@testing-library/react';
 import Footer from './footer';
-import { BrowserRouter } from 'react-router-dom';
+import { render, screen, cleanup, fireEvent } from '@testing-library/react';
+import { BrowserRouter, Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
 
-beforeEach(() => {
-  render(<Footer />, { wrapper: BrowserRouter });
-});
 afterEach(cleanup);
 
 describe('Footer', () => {
   test('Should render with text content', () => {
+    render(<Footer />, { wrapper: BrowserRouter });
     expect(screen.getByTestId('footer')).toBeInTheDocument();
     expect(
       screen.getByText('was developed by Hack for LA')
@@ -18,8 +17,21 @@ describe('Footer', () => {
   });
 
   test('Should exist the tooltip', () => {
+    render(<Footer />, { wrapper: BrowserRouter });
     expect(screen.getByTestId('tooltip')).toBeInTheDocument();
     expect(screen.getByTestId('link')).toBeInTheDocument();
     expect(screen.getByTestId('link')).toHaveAttribute('href', '/page');
+  });
+
+  test('Should navigate to dummy page after clicking on the link', () => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={history}>
+        <Footer />
+      </Router>
+    );
+    expect(screen.getByTestId('link')).toBeInTheDocument();
+    fireEvent.click(screen.getByTestId('link'));
+    expect(history.location.pathname).toBe('/page');
   });
 });
