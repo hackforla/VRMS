16f0556319ea5c889491d210a34a5b4f108ed5a7
OlgaBelogurova <obelogurova2@gmail.com>
2020-10-26 15:36:33 -0700

    Add test to Header regarding navigation to the home onClick

--- client-mvp-04/src/components/common/header/header.test.js
+++ client-mvp-04/src/components/common/header/header.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
 import Header from './header';
-import { BrowserRouter } from 'react-router-dom';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { BrowserRouter, Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
 
 describe('Header', () => {
   test('Should render with logo', () => {
@@ -9,5 +10,18 @@ describe('Header', () => {
     expect(screen.getByTestId('header')).toBeInTheDocument();
     expect(screen.getByTestId('logo')).toBeInTheDocument();
     expect(screen.getByTestId('link')).toHaveAttribute('href', '/');
+  });
+
+  test('Should navigate to home page after clicking on the logo', () => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={history}>
+        <Header />
+      </Router>
+    );
+    expect(screen.getByTestId('logo')).toBeInTheDocument();
+    expect(screen.getByTestId('link')).toHaveAttribute('href', '/');
+    fireEvent.click(screen.getByTestId('logo'));
+    expect(history.location.pathname).toBe('/');
   });
 });
