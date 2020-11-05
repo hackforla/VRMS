46970324386ebd7cba4f71b08f8b5934a1451e13
OlgaBelogurova <obelogurova2@gmail.com>
2020-11-05 14:22:53 -0800

    Create types for activate/disable elements and reset state

--- client-mvp-04/src/store/actions/types.js
+++ client-mvp-04/src/store/actions/types.js
@@ -1,2 +1,10 @@
 // Auth
-export const LOGIN = "LOGIN";
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAIL = 'LOGIN_FAIL';
+
+// Activate/Disable Element
+export const ACTIVATE_EL = 'ACTIVATE_ELEMENT';
+export const DISABLE_EL = 'DISABLE_ELEMENT';
+
+// Reset State
+export const RESET_STATE = 'RESET_STATE';
