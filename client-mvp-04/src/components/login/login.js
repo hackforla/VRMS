0c88922f16b3acfd78226f7b1a37b537d19175a0
OlgaBelogurova <obelogurova2@gmail.com>
2020-11-05 14:31:13 -0800

    Create Login component, connect it to redux store, change button state on input onChange

--- client-mvp-04/src/components/login/login.js
+++ client-mvp-04/src/components/login/login.js
@@ -4,21 +4,54 @@ import Button from '../common/button/button';
 import RedirectLink from '../common/link/link';
 import Title from '../common/title/title';
 import Input from '../common/input/input';
+import { connect } from 'react-redux';
+import {
+  disableElement,
+  activateElement,
+} from '../../store/actions/elementActions';
 
-const Login = () => {
+const Login = (props) => {
+  const { disabledBtn } = props;
+
+  function handleInputChange(e) {
+    const email = e.currentTarget.value.toString();
+    if (email) {
+      props.dispatch(activateElement());
+    } else {
+      props.dispatch(disableElement());
+    }
+  }
+
   return (
     <section data-testid="login" className="login-container">
       <Title />
 
-      <Input placeholder={'Enter your email'} type={'email'} />
+      <Input
+        placeholder={'Enter your email'}
+        type={'email'}
+        onChange={(e) => handleInputChange(e)}
+      />
 
       <RedirectLink
         path={'/page'}
-        content={<Button content={`Sign in`} className={'login-button'} />}
+        content={
+          <Button
+            content={`Sign in`}
+            className={'login-button'}
+            disabled={disabledBtn}
+          />
+        }
         linkKey={'login-link'}
       />
     </section>
   );
 };
 
-export default Login;
+const mapStateToProps = function (state) {
+  return {
+    loggedIn: state.auth.loggedIn,
+    disabledBtn: state.element.disabled,
+  };
+};
+
+export default connect(mapStateToProps)(Login);
