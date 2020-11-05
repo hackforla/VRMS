ae35af878d1551a6332d7702c4954b16a69d5733
OlgaBelogurova <obelogurova2@gmail.com>
2020-11-05 14:23:33 -0800

    Create resetActions and elementActions

---
+++ client-mvp-04/src/store/actions/elementActions.js
@@ -0,0 +1,4 @@
+import { ACTIVATE_EL, DISABLE_EL } from './types';
+
+export const activateElement = () => ({ type: ACTIVATE_EL });
+export const disableElement = () => ({ type: DISABLE_EL });
---
+++ client-mvp-04/src/store/actions/resetActions.js
@@ -0,0 +1,3 @@
+import { RESET_STATE } from './types';
+
+export const resetState = () => ({ type: RESET_STATE });
