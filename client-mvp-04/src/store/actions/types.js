b313b8238e9d335a88f20fa54b4f522800a2602d
OlgaBelogurova <obelogurova2@gmail.com>
2020-11-09 12:39:13 -0800

    Add new types to store

--- client-mvp-04/src/store/actions/types.js
+++ client-mvp-04/src/store/actions/types.js
@@ -1,6 +1,10 @@
-// Auth
+// AUTH
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAIL = 'LOGIN_FAIL';
+
+// USER
+export const SET_USER = 'SET_USER';
+export const FAIL_USER = 'FAIL_USER';
 
 // Reset State
 export const RESET_STATE = 'RESET_STATE';
