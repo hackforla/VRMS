3a67437b44c4151bb96ee3b0503a74bff9b75fe9
OlgaBelogurova <obelogurova2@gmail.com>
2020-11-09 17:47:06 -0800

    Add custom headers

--- client-mvp-04/src/services/user.service.js
+++ client-mvp-04/src/services/user.service.js
@@ -1,11 +1,14 @@
+const headers = {
+  'Content-Type': 'application/json',
+  'x-customrequired-header': process.env.REACT_APP_CUSTOM_REQUEST_HEADER,
+};
+
 const UserService = {
   async getData(email) {
     try {
       const response = await fetch('/api/checkuser', {
         method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
+        headers: headers,
         body: JSON.stringify({ email: email }),
       });
       return await response.json();
