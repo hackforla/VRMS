1d5ca3ff2323683d19b574a3f440d2b3545cd98d
OlgaBelogurova <obelogurova2@gmail.com>
2020-11-12 17:03:22 -0800

    Extract test for email validation from loginContainer.test.js to validation.test.js

--- client-mvp-04/src/components/login/loginContainer.test.js
+++ client-mvp-04/src/components/login/loginContainer.test.js
@@ -6,7 +6,6 @@ import { BrowserRouter } from 'react-router-dom';
 import { cleanup, fireEvent, render, screen } from '@testing-library/react';
 import authReducer from '../../store/reducers/authReducer';
 import userReducer from '../../store/reducers/userReducer';
-import { Email } from '../../utils/validation';
 
 const mockStore = configureStore([]);
 let store = mockStore({
@@ -45,39 +44,6 @@ describe('Login Container', () => {
       expect(screen.getByText('Sign in')).not.toBeDisabled();
       fireEvent.change(loginInput, { target: { value: 'test' } });
       expect(screen.getByText('Sign in')).not.toBeDisabled();
-    });
-  });
-
-  describe('Valid/Invalid Email', () => {
-    test('Should be invalid after check using RegExp', () => {
-      let isEmailValid = false;
-      expect(isEmailValid).toBeFalsy();
-
-      isEmailValid = Email.isValid('test');
-      expect(isEmailValid).toBeFalsy();
-
-      isEmailValid = Email.isValid('test@gmail');
-      expect(isEmailValid).toBeFalsy();
-
-      isEmailValid = Email.isValid('test@gmail.c');
-      expect(isEmailValid).toBeFalsy();
-    });
-
-    test('Should be valid after check using RegExp', () => {
-      let isEmailValid = false;
-      expect(isEmailValid).toBeFalsy();
-
-      isEmailValid = Email.isValid('test@gmail.co');
-      expect(isEmailValid).toBeTruthy();
-
-      isEmailValid = Email.isValid('test@gmail.com');
-      expect(isEmailValid).toBeTruthy();
-
-      isEmailValid = Email.isValid('test.test@gmail.com');
-      expect(isEmailValid).toBeTruthy();
-
-      isEmailValid = Email.isValid('test55@gmail.com');
-      expect(isEmailValid).toBeTruthy();
     });
   });
 
