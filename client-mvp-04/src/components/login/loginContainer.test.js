418238a40a1523172d2764014744e5d3d7684df3
OlgaBelogurova <obelogurova2@gmail.com>
2020-11-14 15:40:25 -0800

    Mock UserService, mock HistoryPush; Add test get user from UserService if user registered in the app

--- client-mvp-04/src/components/login/loginContainer.test.js
+++ client-mvp-04/src/components/login/loginContainer.test.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
 import LoginContainer from './loginContainer';
-import { BrowserRouter } from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom';
 import { cleanup, fireEvent, render, screen } from '@testing-library/react';
 import authReducer from '../../store/reducers/authReducer';
 import userReducer from '../../store/reducers/userReducer';
+import getUser from '../../services/user.service';
 
+// Mock Redux Store
 const mockStore = configureStore([]);
 let store = mockStore({
   auth: authReducer,
@@ -14,18 +16,51 @@ let store = mockStore({
 });
 store.dispatch = jest.fn();
 
+// Mock UserService
+const mockUserData = {
+  name: { firstName: 'Test', lastName: 'Person' },
+  accessLevel: 'user',
+  skillsToMatch: [],
+  projects: [],
+  textingOk: false,
+  _id: '5f4bfbc8e9f4f121e8c1eb42',
+  email: 'test@gmail.com',
+  currentRole: 'College Student',
+  desiredRole: 'Software Developer',
+  newMember: false,
+  attendanceReason: 'Environment',
+  currentProject: 'VRMS',
+  createdDate: '2020-11-11T03:48:46.153Z',
+};
+jest.mock('../../services/user.service', () =>
+  jest.fn().mockImplementationOnce(() => {
+    return mockUserData;
+  })
+);
+
+// Mock history.push('/path')
+const mockHistoryPush = jest.fn();
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useHistory: () => ({
+    push: mockHistoryPush,
+  }),
+}));
+
 beforeEach(() => {
   render(
     <Provider store={store}>
-      <LoginContainer />
-    </Provider>,
-    { wrapper: BrowserRouter }
+      <MemoryRouter initialEntries={['/login']}>
+        <LoginContainer />
+      </MemoryRouter>
+    </Provider>
   );
 });
+
 afterEach(cleanup);
 
 describe('Login Container', () => {
-  describe('Disable/Enable `Sign In` Button', () => {
+  describe('`Sign In` Button', () => {
     test('Should be disabled by default', () => {
       expect(screen.getByText('Sign in')).toBeDisabled();
     });
@@ -47,15 +82,21 @@ describe('Login Container', () => {
     });
   });
 
-  describe('Show/Hide Error', () => {
-    test('Should display the error message if email invalid', () => {
-      const loginInput = screen.getByTestId('login-input');
-      expect(loginInput).toBeInTheDocument();
-      fireEvent.change(loginInput, { target: { value: 'test@gmail.c' } });
-      fireEvent.submit(screen.getByTestId('login-form'));
-      expect(
-        screen.getByText('*Please enter a valid email address')
-      ).toBeInTheDocument();
-    });
+  test('Should display error message if email invalid', () => {
+    const loginInput = screen.getByTestId('login-input');
+    expect(loginInput).toBeInTheDocument();
+    fireEvent.change(loginInput, { target: { value: 'test@gmail.c' } });
+    fireEvent.submit(screen.getByTestId('login-form'));
+    expect(
+      screen.getByText('*Please enter a valid email address')
+    ).toBeInTheDocument();
+  });
+
+  test('Should get user from UserService if user registered in the app', () => {
+    const loginInput = screen.getByTestId('login-input');
+    expect(loginInput).toBeInTheDocument();
+    fireEvent.change(loginInput, { target: { value: 'test@gmail.com' } });
+    fireEvent.submit(screen.getByTestId('login-form'));
+    expect(() => getUser('test@gmail.com').toMatchObject(mockUserData));
   });
 });
