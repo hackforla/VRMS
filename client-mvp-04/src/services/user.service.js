995ba9fba9bf77d99813d3bc171f7e08e59b284c
OlgaBelogurova <obelogurova2@gmail.com>
2020-11-18 21:37:57 -0800

    Add functions checkAuth, authUserWithToken to make API calls

--- client-mvp-04/src/services/user.service.js
+++ client-mvp-04/src/services/user.service.js
@@ -1,11 +1,42 @@
-const HEADERS = {
-  'Content-Type': 'application/json',
-  'x-customrequired-header': process.env.REACT_APP_CUSTOM_REQUEST_HEADER,
-};
+import {
+  HEADERS,
+  CHECK_USER,
+  SIGN_IN,
+  AUTH_VERIFY_TOKEN,
+} from '../utils/endpoints';
 
-export default async function getUser(email) {
+export async function checkAuth(email) {
   try {
-    const response = await fetch('/api/checkuser', {
+    const response = await fetch(SIGN_IN, {
+      method: 'POST',
+      headers: HEADERS,
+      body: JSON.stringify({ email: email }),
+    });
+    return response.status === 200;
+  } catch (error) {
+    console.log('User is not authorized in app');
+    console.log(error);
+    return null;
+  }
+}
+
+export async function authUserWithToken(token) {
+  try {
+    const response = await fetch(AUTH_VERIFY_TOKEN, {
+      method: 'POST',
+      headers: { ...HEADERS, 'x-access-token': token },
+    });
+    return response.status === 200;
+  } catch (error) {
+    console.log('User is not authorized with token');
+    console.log(error);
+    return null;
+  }
+}
+
+export async function getUser(email) {
+  try {
+    const response = await fetch(CHECK_USER, {
       method: 'POST',
       headers: HEADERS,
       body: JSON.stringify({ email: email }),
