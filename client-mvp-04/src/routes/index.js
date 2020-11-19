7ff5fcf5454a33f6025b1272989c5174bee2e2b3
OlgaBelogurova <obelogurova2@gmail.com>
2020-11-19 14:17:04 -0800

    Add new rotes

--- client-mvp-04/src/routes/index.js
+++ client-mvp-04/src/routes/index.js
@@ -1,7 +1,10 @@
 import Home from '../components/home/home';
 import Dummy from '../components/dummy/dummy';
 import Error from '../components/error/error';
-import LoginContainer from '../components/login/loginContainer';
+import Login from '../components/login/loginContainer';
+import Auth from '../components/auth/auth';
+import HandleAuth from '../components/auth/handleAuth';
+import Dashboard from '../components/dashboard/dashboard';
 
 import DevUiKit from '../utils/uiKit/uiKit';
 
@@ -14,7 +17,22 @@ export const Routes = [
   {
     path: '/login',
     key: 'login',
-    component: LoginContainer,
+    component: Login,
+  },
+  {
+    path: '/login/auth',
+    key: 'auth',
+    component: Auth,
+  },
+  {
+    path: '/handleauth',
+    key: 'handleauth',
+    component: HandleAuth,
+  },
+  {
+    path: '/dashboard',
+    key: 'dashboard',
+    component: Dashboard,
   },
   {
     path: '/page',
