137ca4d01afc98d5296d10d03e52bcacd8c260f0
OlgaBelogurova <obelogurova2@gmail.com>
2020-12-10 20:04:32 -0800

    Create getUserWithToken auth async action with middleware Add setUser action

--- client-mvp-04/src/store/actions/authActions.js
+++ client-mvp-04/src/store/actions/authActions.js
@@ -1,4 +1,33 @@
-import { LOGIN_SUCCESS, LOGIN_FAIL } from './types';
+import { AUTH_START, AUTH_SUCCESS, AUTH_FAIL, SET_USER } from './types';
+import { HEADERS, AUTH_VERIFY_SIGN_IN } from '../../utils/endpoints.js';
 
-export const loginSuccess = () => ({ type: LOGIN_SUCCESS });
-export const loginFailed = () => ({ type: LOGIN_FAIL });
+export const authStart = () => ({ type: AUTH_START });
+
+export const authSuccess = (authToken, user) => ({
+  type: AUTH_SUCCESS,
+  payload: {
+    user: user,
+    authToken: authToken,
+  },
+});
+export const authFail = (error) => ({ type: AUTH_FAIL, payload: error });
+
+export const getUserWithToken = (token) => {
+  return async (dispatch) => {
+    dispatch(authStart);
+    try {
+      const response = await fetch(AUTH_VERIFY_SIGN_IN, {
+        method: 'POST',
+        headers: { ...HEADERS, 'x-access-token': token },
+      });
+      const user = await response.json();
+      dispatch(authSuccess(token, user));
+    } catch (error) {
+      console.log('User is not authorized with token');
+      console.log(error);
+      dispatch(authFail(error.message));
+    }
+  };
+};
+
+export const setUser = (user) => ({ type: SET_USER, payload: user });
