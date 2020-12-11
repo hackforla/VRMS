c30b8fe15b95fe571c5fc5f4a6a0e9101f92dc1a
OlgaBelogurova <obelogurova2@gmail.com>
2020-12-10 20:11:16 -0800

    Remove userReducer from rootReducer,change prop for loginContainer

--- client-mvp-04/src/store/reducers/index.js
+++ client-mvp-04/src/store/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import authReducer from './authReducer';
-import userReducer from './userReducer';
 
 const rootReducer = combineReducers({
   auth: authReducer,
-  user: userReducer,
 });
 
 export default rootReducer;
