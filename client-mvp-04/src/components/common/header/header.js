859385e6f5019e1462142128dfc54562b3bcdb6f
OlgaBelogurova <obelogurova2@gmail.com>
2020-12-15 09:49:24 -0800

    Create the menu button inside the header, add styles

--- client-mvp-04/src/components/common/header/header.js
+++ client-mvp-04/src/components/common/header/header.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './header.scss';
 import logo from '../../../assets/images/logo.svg';
 import RedirectLink from '../link/link';
 
 const Header = () => {
+  const [isOpenMenu, setOpenMenu] = useState(false);
+
   return (
     <header data-testid="header" className="app-header">
+      <div className="menu-button-container">
+        <div
+          className={isOpenMenu ? 'active menu-button' : 'menu-button'}
+          onClick={() => setOpenMenu(!isOpenMenu)}
+        >
+          <span className="line" />
+          <span className="line" />
+          <span className="line" />
+        </div>
+      </div>
+
       <RedirectLink
         linkKey={'header-home-link'}
         path={'/'}
