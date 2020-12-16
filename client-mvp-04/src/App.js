ae0f52723409c45d81514fe4151761e311745294
OlgaBelogurova <obelogurova2@gmail.com>
2020-12-15 18:33:55 -0800

    Add Menu to the App Component

--- client-mvp-04/src/App.js
+++ client-mvp-04/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import Header from './components/common/header/header';
 import Footer from './components/common/footer/footer';
@@ -6,15 +6,24 @@ import { Provider } from 'react-redux';
 import configureStore from './store/store';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Routes } from './routes';
+import Menu from './components/menu/menu';
 
 const App = () => {
   const store = configureStore();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  function toggleMenu() {
+    setIsMenuOpen(!isMenuOpen);
+  }
+
   return (
     <Provider store={store}>
       <Router>
         <div className="app">
           <div className="app-container">
-            <Header />
+            <Header toggleMenu={toggleMenu} isMenuOpen={isMenuOpen} />
+
+            <Menu toggleMenu={toggleMenu} isMenuOpen={isMenuOpen} />
 
             <main data-testid="main" role="main" className="app-main">
               <Switch>
