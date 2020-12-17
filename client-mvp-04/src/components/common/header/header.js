decf2308241054599876b8af5efc626cdbac8e62
OlgaBelogurova <obelogurova2@gmail.com>
2020-12-16 20:40:17 -0800

    Save to store menu state, show menu button if user logged in

--- client-mvp-04/src/components/common/header/header.js
+++ client-mvp-04/src/components/common/header/header.js
@@ -2,21 +2,41 @@ import React from 'react';
 import './header.scss';
 import logo from '../../../assets/images/logo.svg';
 import RedirectLink from '../link/link';
+import { useHistory } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import allActions from '../../../store/actions';
 
-const Header = ({ toggleMenu, isMenuOpen }) => {
+const Header = () => {
+  const history = useHistory();
+  const isMenuOpen = useSelector((state) => state.dashboard.isMenuOpen);
+  const loggedIn = useSelector((state) => state.auth.loggedIn);
+  const user = useSelector((state) => state.auth.user);
+  const dispatch = useDispatch();
+
+  function toggleMenu() {
+    if (!isMenuOpen) {
+      dispatch(allActions.dashboardActions.openMenu());
+      history.push('/dashboard/menu');
+    } else {
+      dispatch(allActions.dashboardActions.closeMenu());
+      history.goBack();
+    }
+  }
+
   return (
     <header data-testid="header" className="app-header">
-      {/* Show for authorized user */}
-      <div className="menu-button-container">
-        <div
-          className={isMenuOpen ? 'active menu-button' : 'menu-button'}
-          onClick={() => toggleMenu()}
-        >
-          <span className="line" />
-          <span className="line" />
-          <span className="line" />
+      {loggedIn && user ? (
+        <div className="menu-button-container">
+          <div
+            className={isMenuOpen ? 'active menu-button' : 'menu-button'}
+            onClick={() => toggleMenu()}
+          >
+            <span className="line" />
+            <span className="line" />
+            <span className="line" />
+          </div>
         </div>
-      </div>
+      ) : null}
 
       <RedirectLink
         linkKey={'header-home-link'}
