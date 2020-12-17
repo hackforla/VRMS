f5ac760db8edbe301ac932d2ae6252286e2cf03b
OlgaBelogurova <obelogurova2@gmail.com>
2020-12-16 20:30:41 -0800

    Create types, actions, reducer for dashboard

--- client-mvp-04/src/store/actions/types.js
+++ client-mvp-04/src/store/actions/types.js
@@ -3,3 +3,7 @@ export const AUTH_START = 'AUTH_START';
 export const AUTH_SUCCESS = 'AUTH_SUCCESS';
 export const AUTH_FAIL = 'AUTH_FAIL';
 export const SET_USER = 'SET_USER';
+
+// DASHBOARD - MENU
+export const OPEN_MENU = 'SET_OPEN_MENU';
+export const CLOSE_MENU = 'SET_CLOSE_MENU';
