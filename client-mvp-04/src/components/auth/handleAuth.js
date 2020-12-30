0a972f5782f0e7eb065322857b80fc4ca3ccf356
OlgaBelogurova <obelogurova2@gmail.com>
2020-12-30 14:47:16 -0800

    Redirect user to ExpiredAuthSessionPage it token is expired

--- client-mvp-04/src/components/auth/handleAuth.js
+++ client-mvp-04/src/components/auth/handleAuth.js
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Loader from '../../components/common/loader/loader';
 import { Redirect } from 'react-router-dom';
-import RedirectLink from '../common/link/link';
 import allActions from '../../store/actions';
 
 const HandleAuth = (props) => {
@@ -22,15 +21,7 @@ const HandleAuth = (props) => {
     loggedIn && user ? (
       <Redirect to="/dashboard" />
     ) : (
-      <div className="flex-container">
-        <h2>Sorry, this link is not valid</h2>
-        <RedirectLink
-          linkKey={'auth-link'}
-          path={'/'}
-          className={'accent-link'}
-          content={'Go to Homepage'}
-        />
-      </div>
+      <Redirect to="/auth/expired-session" />
     )
   ) : (
     <Loader />
