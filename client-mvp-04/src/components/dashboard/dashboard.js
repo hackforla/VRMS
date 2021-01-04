e7e10170bcd2e0d16dd2862d2f7d68151eb04acb
OlgaBelogurova <obelogurova2@gmail.com>
2021-01-04 15:10:10 -0800

    Define userProfile object in auth state

--- client-mvp-04/src/components/dashboard/dashboard.js
+++ client-mvp-04/src/components/dashboard/dashboard.js
@@ -6,10 +6,11 @@ import { Redirect } from 'react-router-dom';
 const Dashboard = () => {
   const loggedIn = useSelector((state) => state.auth.loggedIn);
   const user = useSelector((state) => state.auth.user);
+  const userProfile = useSelector((state) => state.auth.userProfile);
 
   return loggedIn && user ? (
     <div className="flex-container dashboard">
-      <h2>Hi {user.name.firstName},</h2>
+      <h2>Hi {userProfile.firstName},</h2>
       <br />
       <h2>Welcome to VRMS Dashboard!</h2>
     </div>
