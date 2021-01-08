f18023b8a7734c210e8e1a43c1cd93253a3c20e1
OlgaBelogurova <obelogurova2@gmail.com>
2021-01-07 16:39:07 -0800

    Add styles to Notifications, add redirect on the dummy page if user clicks on check-in button

--- client-mvp-04/src/components/notifications/notifications.js
+++ client-mvp-04/src/components/notifications/notifications.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import './notifications.scss';
 import Button from '../common/button/button';
+import { useHistory } from 'react-router-dom';
 
 const Notifications = () => {
+  const history = useHistory();
+
   // While API for notifications is not implemented, uses mock data
   const dateValue1 = new Date().toLocaleDateString('en-US', {
     month: '2-digit',
@@ -18,13 +21,17 @@ const Notifications = () => {
 
   return (
     <>
-      <h3>Notifications</h3>
+      <h3 className="notifications-header">Notifications</h3>
       <div className="notifications">
-        <div className="project-data">
+        <div className="event-data">
           <div>{`${notifications.projectName} ${notifications.meetingName}`}</div>
           <div>{`${notifications.startTime} ${notifications.endTime}`}</div>
         </div>
-        <Button content={'CHECK IN'} className={'btn-accent'} />
+        <Button
+          content={'CHECK IN'}
+          className={'btn-accent checkin-btn'}
+          onClick={() => history.push('/page')}
+        />
       </div>
     </>
   );
