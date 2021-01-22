f5e7d300191d7ee1c807ddaef234387553b0959e
OlgaBelogurova <obelogurova2@gmail.com>
2021-01-21 19:18:00 -0800

    Add test to ProgressBar component

--- client-mvp-04/src/components/common/progressBar/progressBar.js
+++ client-mvp-04/src/components/common/progressBar/progressBar.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 const ProgressBar = ({ total, active }) => {
   let items = new Array(total).fill(0);
+  if (active > items.length) active = items.length;
 
   return (
-    <div className="progress-bar-container">
+    <div className="progress-bar-container" data-testid="progress-bar">
       {items.map((item, index) => (
         <span
           className={index < active ? 'progress-item active' : 'progress-item'}
           key={index}
+          data-testid="progress-item"
         />
       ))}
     </div>
