0ebc0bc820cd39214dddcaae5f5febad8fffb70e
OlgaBelogurova <obelogurova2@gmail.com>
2021-02-01 11:03:51 -0800

    Implemented reusable component 'Popup'

---
+++ client-mvp-04/src/components/common/popup/popup.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './popup.scss';
+
+const Popup = ({ content, isPopupOpen, closePopup }) => {
+  return (
+    <div
+      className={isPopupOpen ? 'bg-overlay active' : 'bg-overlay'}
+      onClick={(e) => closePopup(e)}
+    >
+      <div className={isPopupOpen ? 'popup-container open' : 'popup-container'}>
+        <div className="button-container">
+          <div className="close-button" id="popup-close-btn">
+            <span className="line" />
+            <span className="line" />
+          </div>
+        </div>
+
+        <div className="popup-content">{content}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Popup;
