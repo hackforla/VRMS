f50dbcb3bdf93c96f9f723006a872f27b446895a
OlgaBelogurova <obelogurova2@gmail.com>
2021-02-04 20:32:23 -0800

    Created codeOfConduct markdown mock data

---
+++ client-mvp-04/src/utils/testUtils/mocks/dataMock.js
@@ -0,0 +1,18 @@
+/**
+ * Code of Conduct mock data
+ */
+export const CodeOfConductMarkdown = `
+    "# Hack for LA's Code of Conduct\\n" +
+    '[![Slack]()\\n' +
+    '\\n' +
+    'This document is forked from the [The Code for America Code of Conduct](https://github.com/).\\n' +
+    '\\n' +
+    'Hack for LA expects that all network activities, events, and digital forums:\\n' +
+    '\\n' +
+    '1. Are a safe and respectful environment for all participants.\\n' +
+    '1. Are a place where people are free to fully express their identities.\\n' +
+    '\\n' +
+    'We value your attendance and hope that by communicating these expectations widely we can all enjoy a harassment-free environment.\\n' +
+    '\\n' +
+    '### Email Template for Anti-Harassment Reporting',
+    `;
