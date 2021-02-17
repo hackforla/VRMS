18679a6f602da193989e77328b15ff90dd7e00a2
OlgaBelogurova <obelogurova2@gmail.com>
2021-02-16 23:46:15 -0800

    Add auth_origin for checkUser API, auto code formatting

--- backend/routers/checkUser.router.js
+++ backend/routers/checkUser.router.js
@@ -7,46 +7,44 @@ const { User } = require('../models/user.model');
 
 // GET /api/checkuser/
 router.post('/', (req, res) => {
-    const { email } = req.body;
-    console.log(email);
-    
-    if(email === "undefined") {
+  const { email, auth_origin } = req.body;
+  console.log(email);
+  console.log(`auth_origin: ${auth_origin}`);
+
+  if (email === 'undefined') {
+    return res.sendStatus(400);
+  }
+
+  if (email) {
+    User.findOne({ email })
+      .then((user) => {
+        if (!user) {
+          return res.sendStatus(400);
+        } else {
+          return res.status(200).send(user);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+
         return res.sendStatus(400);
-    }
-
-    if(email) {
-        User
-            .findOne({email})
-            .then(user => {
-                if (!user) {
-                    return res.sendStatus(400);
-                } else {
-                    return res.status(200).send(user);
-                }
-            })
-            .catch(err => {
-                console.log(err);
-
-                return res.sendStatus(400);
-            });
-    } else {
-      // TODO: Refactor as path is not called, or tested.
-      res.json({ message: 'Enter the email address you used to check-in last time.' });
-    }
-    
+      });
+  } else {
+    // TODO: Refactor as path is not called, or tested.
+    res.json({ message: 'Enter the email address you used to check-in last time.' });
+  }
 });
 
 router.get('/:id', (req, res) => {
-    // TODO: Refactor and test
-    User
-        .findById(req.params.id)
-        .then(user => {
-            return res.status(200).send(user);
-        })
-        .catch(err => {
-            console.log(err);
-            res.sendStatus(400);
-        });
+  // TODO: Refactor and test
+  User.findById(req.params.id)
+    .then((user) => {
+      return res.status(200).send(user);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(400);
+    });
 });
 
 module.exports = router;
