ee0ccd7fade408cd410c6bf3ba8570c80fae5402
Richard Cook <awhig@yahoo.com>
2021-02-17 13:37:37 -0800

    Implement a service for operating TimeZone entity #459

--- backend/services/index.js
+++ backend/services/index.js
@@ -1,7 +1,9 @@
 const { ModificationLogService } = require('./modificationLog.service');
 const { UserProfileService } = require('./userProfile.service');
+const { TimeZoneService } = require('./timeZone.service');
 
 module.exports = {
   ModificationLogService,
-  UserProfileService
+  UserProfileService,
+  TimeZoneService
 };
