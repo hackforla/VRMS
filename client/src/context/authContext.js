cd2b6b339ffdb8478c85f4ed80c490a6b849093e
Amy Mendelsohn <mendelsohn.amy@gmail.com>
2021-04-30 21:20:28 -0700

    cleanup: fix warnings from unused imports, unused vars

--- client/src/context/authContext.js
+++ client/src/context/authContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext } from 'react';
 
 import useProvideAuth from '../hooks/useProvideAuth';
 
--- client/src/pages/EmailSent.js
+++ client/src/pages/EmailSent.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-// import { Link } from 'react-router-dom';
 
 import '../sass/MagicLink.scss';
 
