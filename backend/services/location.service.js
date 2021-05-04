7c2e4aa72d42882b593d10e678cbdd119d2e1c15
Akib Mohaimenur Rahman <akib.rhast@gmail.com>
2021-05-04 03:20:54 -0400

    Applied changes requested

--- backend/services/location.service.js
+++ backend/services/location.service.js
@@ -4,18 +4,40 @@ const DatabaseError = require('../errors/database.error');
 
 const LocationService = {};
 
+function validateLocations(locationsarray){
+
+  if(!Array.isArray(locationsarray)){
+    throw "locations array must be of type array"
+  }
+  if(locationsarray.length <= 0){
+    throw "locations array must have at least one element";
+  }
+  if( !locationsarray.every(item => (typeof item === "string")) ){
+    throw "locations array must be an array of strings"
+  }
+  if( !locationsarray.every( (element) => { if(element){return true} } )){
+    throw "locations parameter is an array of non-empty strings"
+  }
+
+
+}
 LocationService.add = async function (locations) {
-  try {
+  try{
+
+    validateLocations(locations);
     await Location.findOneAndUpdate(
       {},
       {
         $addToSet: { locations: { $each: locations } },
       },
-      {new:true, upsert: true, runValidators: true, context: 'query' },
+      {new:true, upsert: true, context: 'query' },
     );
-  } catch (error) {
-    throw new DatabaseError(error.message);
+
   }
+  catch(error){
+    throw new DatabaseError(error);
+  }
+
 };
 
 LocationService.getAll = async function () {
