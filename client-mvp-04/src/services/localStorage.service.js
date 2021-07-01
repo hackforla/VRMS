921d75da79bfad9e778dcf96730dee58a8a20622
Md. Pial Ahamed <root.pial@gmail.com>
2021-07-01 23:30:54 +0600

    Add functions to use local storage of the browser

---
+++ client-mvp-04/src/services/localStorage.service.js
@@ -0,0 +1,53 @@
+/**
+ * Sets value in the local storage.
+ * @param {string} key - A string specifying the name of the key you want to set the value of.
+ * @param {string} value - A string specifying the value of the key.
+ */
+export function setItem(key, value) {
+  try {
+    window.localStorage.setItem(key, value);
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.error(err);
+  }
+}
+
+/**
+ * Gets value from the local storage
+ * @param {string} key A string specifying the name of the key you want to get the value of
+ * @return {string} A string contains the value of the key
+ */
+// eslint-disable-next-line consistent-return
+export function getItem(key) {
+  try {
+    return window.localStorage.getItem(key);
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.error(err);
+  }
+}
+
+/**
+ * Removes an item by key from the local storage.
+ * @param {string} key - A string specifying the name of the key you want to delete.
+ */
+export function removeItem(key) {
+  try {
+    window.localStorage.removeItem(key);
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.error(err);
+  }
+}
+
+/**
+ * Clears all items from the local storage for the domain.
+ */
+export function clear() {
+  try {
+    window.localStorage.clear();
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.error(err);
+  }
+}
