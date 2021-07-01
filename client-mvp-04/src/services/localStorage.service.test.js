a43d982194ee405ac56024b8d804a97de1704750
Md. Pial Ahamed <root.pial@gmail.com>
2021-07-02 00:22:56 +0600

    Update unit tests for the functions of localStorage.service.js

--- client-mvp-04/src/services/localStorage.service.test.js
+++ client-mvp-04/src/services/localStorage.service.test.js
@@ -2,25 +2,38 @@
 import { setItem, getItem, removeItem, clear } from './localStorage.service';
 
 describe('Tests Functions of the localStorage.service.js', () => {
-  it('sets and gets the given value from the local storage', () => {
-    const givenValue = 'this is a demo value';
+  it('sets and gets the given object value from the local storage', () => {
+    const givenValue = {
+      name: 'VRMS',
+      location: 'LA',
+    };
+    setItem('vrms', givenValue);
+    expect(getItem('vrms')).toEqual(givenValue);
+  });
+
+  it('sets and gets the given string value from the local storage', () => {
+    const givenValue = 'This is a demo string value';
     setItem('demo', givenValue);
     expect(getItem('demo')).toEqual(givenValue);
   });
-  it('removes the item from the local storage', () => {
+
+  it('removes item from the local storage', () => {
     removeItem('demo');
     expect(getItem('demo')).toEqual(null);
   });
+
   it('clears all items from the local storage', () => {
-    setItem('one', '1');
-    setItem('two', '2');
-    setItem('three', '3');
-    expect(getItem('one')).toEqual('1');
-    expect(getItem('two')).toEqual('2');
-    expect(getItem('three')).toEqual('3');
+    const stringValue = 'Hello World!';
+    const objectValue = {
+      type: 'Alien',
+      currentLocation: 'Earth',
+    };
+    setItem('string', stringValue);
+    setItem('object', objectValue);
+    expect(getItem('string')).toEqual(stringValue);
+    expect(getItem('object')).toEqual(objectValue);
     clear();
     expect(getItem('one')).toEqual(null);
     expect(getItem('two')).toEqual(null);
-    expect(getItem('three')).toEqual(null);
   });
 });
