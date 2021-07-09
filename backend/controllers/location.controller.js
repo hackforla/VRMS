a4d157e5c8bcda1a5a516590c3d0a89819ef364f
Glen Paul Florendo <9373317+glenflorendo@users.noreply.github.com>
2021-07-08 19:25:10 -0700

    Refactor locations controller to replace try-catch blocks

--- backend/controllers/location.controller.js
+++ backend/controllers/location.controller.js
@@ -3,21 +3,17 @@ const { LocationService } = require('../services');
 const LocationController = {};
 
 LocationController.location_list = async (req, res, next) => {
-    try {
-        const locations = await LocationService.getAll();
-        return res.status(200).json(locations);
-    } catch (error) {
-        next(error)
-    }
+    await LocationService
+        .getAll()
+        .then(locations => res.status(200).json(locations))
+        .catch(next);
 }
 
 LocationController.create = async (req, res, next) => {
-    try {
-        await LocationService.add(req.body.locations);
-        return res.status(201).json();
-    } catch (error) {
-        next(error)
-    }
+    await LocationService
+        .add(req.body.locations)
+        .then(() => res.status(201).json())
+        .catch(next);
 }
 
 module.exports = LocationController;
