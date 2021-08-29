3685b7462bfdac6a31208a6384de3130c11860e2
Jason Wong <jasonwong.26@gmail.com>
2021-08-29 14:10:02 -0700

    removed test code from healthcheck page

--- client/src/pages/HealthCheck.js
+++ client/src/pages/HealthCheck.js
@@ -1,18 +1,9 @@
 import React from "react";
-import { REACT_APP_CUSTOM_REQUEST_HEADER } from "../utils/globalSettings";
-
-const customHeader = REACT_APP_CUSTOM_REQUEST_HEADER;
-const reactProxy = process.env.REACT_APP_PROXY;
 
 const HealthCheck = () => {
   return (
     <div className="flex-container text-center">
         <h3 style={{"text-align": "center"}}>"I'm Alive!"</h3>
-        <hr />
-        <div>
-          <div><strong>REACT_APP_CUSTOM_REQUEST_HEADER</strong>: {customHeader}</div>
-          <div><strong>REACT_APP_PROXY</strong>: {reactProxy}</div>
-        </div>
     </div>
   );
 };
