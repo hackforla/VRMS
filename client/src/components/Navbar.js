b1da27cbc18dbe370164ec7de51b91e5378aba0b
jbubar <jpbubar@gmail.com>
2021-10-11 01:18:26 -0700

    change nav to always show the same links, and add 'add event' btn on even page

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -13,82 +13,63 @@ const Navbar = (props) => {
     let loginRedirect = '/admin'; 
     if (auth?.user) {
       loginRedirect = authLevelRedirect(auth.user);
+      console.log("auth User!: ",auth.user)
     }
-
     return (
-        <div className="nav-wrapper">
-            <nav
-                className="navbar"
-                role="navigation"
-                aria-label="main navigation"
+      <div className="nav-wrapper">
+        <nav className="navbar" role="navigation" aria-label="main navigation">
+          <div className="navbar-buttons-container">
+            {!auth?.user ? (
+              <>
+                <Link to="/">
+                  <p className="home-link-text">HOME</p>
+                </Link>
+                <Link to={loginRedirect}>
+                  <p className="home-link-text">LOGIN</p>
+                </Link>
+              </>
+            ) : null}
+            {auth?.user?.accessLevel === 'admin' ? (
+              <>
+                <Link to="/admin">
+                  <p className="home-link-text">HOME</p>
+                </Link>
+                <Link to="/events">
+                  <p className="home-link-text">EVENTS</p>
+                </Link>
+                <Link to="/useradmin">
+                  <p className="home-link-text">ADMIN</p>
+                </Link>
+                <Link to="/projects">
+                  <p className="home-link-text">PROJECTS</p>
+                </Link>
+              </>
+            ) : null}
+
+            {auth?.user?.accessLevel === 'user' ? (
+              <Link to="/projects">
+                <p className="home-link-text">PROJECTS</p>
+              </Link>
+            ) : null}
+          </div>
+
+          {props.location.pathname === '/' ||
+          props.location.pathname === '/success' ? (
+            <div className="navbar-logo grow">
+              <img src="/hflalogo.png" alt="Hack for LA Logo"></img>
+            </div>
+          ) : (
+            <div
+              className={`navbar-logo ${
+                props.location.pathname === '/admin' && 'justify-right'
+              }`}
             >
-                <div className="navbar-buttons-container">
-                    {props.location.pathname === "/" ? (
-                        <Link to={loginRedirect}>
-                            <p className="home-link-text">LOGIN</p>
-                        </Link>
-                    ) : (
-                        <Link to="/">
-                            <p className="home-link-text">HOME</p>
-                        </Link>
-                    )}
-
-                    {props.location.pathname === "/projectmanager" ? (
-                        <Link to="/projects">
-                            <p className="home-link-text">PROJECTS</p>
-                        </Link>
-                    ) : null}
-
-                    {props.location.pathname === "/admin" ? (
-                        <>
-                            <Link to="/events">
-                                <p className="home-link-text">EVENTS</p>
-                            </Link>
-                            <Link to="/add/event">
-                                <p className="home-link-text">ADD EVENT</p>
-                            </Link>
-                            <Link to="/useradmin">
-                                <p className="home-link-text">ADMIN</p>
-                            </Link>
-                            <Link to="/projects">
-                                <p className="home-link-text">PROJECTS</p>
-                            </Link>
-                        </>
-                    ) : null}
-
-                    {props.location.pathname ===  "/useradmin" ? (
-                        <>
-                            <Link to="/events">
-                                <p className="home-link-text">EVENTS</p>
-                            </Link>
-                            <Link to="/useradmin">
-                                <p className="home-link-text">ADMIN</p>
-                            </Link>
-                            <Link to="/projects">
-                                <p className="home-link-text">PROJECTS</p>
-                            </Link>
-                        </>
-                    ) : null}
-                </div>
-
-                {props.location.pathname === "/" ||
-                props.location.pathname === "/success" ? (
-                    <div className="navbar-logo grow">
-                        <img src="/hflalogo.png" alt="Hack for LA Logo"></img>
-                    </div>
-                ) : (
-                    <div
-                        className={`navbar-logo ${
-                            props.location.pathname === "/admin" &&
-                            "justify-right"
-                        }`}
-                    >
-                        <img src="/hflalogo.png" alt="Hack for LA Logo"></img>
-                    </div>
-                )}
-            </nav>
-        </div>
+              <img src="/hflalogo.png" alt="Hack for LA Logo"></img>
+            </div>
+          )}
+        </nav>
+      </div>
     );
 };
-
+    
 export default withRouter(Navbar);
