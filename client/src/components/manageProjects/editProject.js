c489e42e03217322f2a9acaa02ac21b57f552c86
Katiuska Alicea <katiuskaalicea@gmail.com>
2021-12-11 14:29:56 -0800

    added state in editableField.js for user access level to implement conditional renderng

--- client/src/components/manageProjects/editProject.js
+++ client/src/components/manageProjects/editProject.js
@@ -1,207 +1,226 @@
-import React, { useState }  from 'react';
+import React, { useState } from 'react';
 import '../../sass/ManageProjects.scss';
 import EditableField from './editableField';
-import { REACT_APP_CUSTOM_REQUEST_HEADER } from "../../utils/globalSettings";
+import { REACT_APP_CUSTOM_REQUEST_HEADER } from '../../utils/globalSettings';
 
+// Need to hold user state to check which type of user they are and conditionally render editing fields in this component
+//for user level block access to all except for the ones checked
 
-const EditProjectInfo  = ( props ) => {
+const EditProject = (props) => {
+  console.log('editProject.js: props:', props);
 
   const headerToSend = REACT_APP_CUSTOM_REQUEST_HEADER;
 
   //const [currentProjectData, setCurrentProjectData] = useState(props.projectToEdit);
 
   const updateProject = async (projectId, fieldName, fieldValue) => {
-
     // These field are arrays, but the form makes them comma separated strings, so this adds it back to db as an arrray.
-    if (fieldName === 'partners' || fieldName === 'recruitingCategories') {  
+    if (fieldName === 'partners' || fieldName === 'recruitingCategories') {
       if (!Array.isArray(fieldValue)) {
         fieldValue = fieldValue
-        .split(',')
-        .filter(x => x !== "")
-        .map(y => y.trim());
+          .split(',')
+          .filter((x) => x !== '')
+          .map((y) => y.trim());
       }
     }
-    
+
     // Update database
     const url = `/api/projects/${projectId}`;
     const requestOptions = {
-        method: 'PATCH',
-        headers: {
-            "Content-Type": "application/json",
-            "x-customrequired-header": headerToSend
-        },
-        body: JSON.stringify({ [fieldName]: fieldValue })
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        'x-customrequired-header': headerToSend,
+      },
+      body: JSON.stringify({ [fieldName]: fieldValue }),
     };
 
     try {
-        const response = await fetch(url, requestOptions); 
-        const resJson = await response.json();
+      const response = await fetch(url, requestOptions);
+      const resJson = await response.json();
 
-        return resJson;
+      return resJson;
     } catch (error) {
-        console.log(`update user error: `, error);
-        alert("Server not responding.  Please try again.");
+      console.log(`update user error: `, error);
+      alert('Server not responding.  Please try again.');
     }
   };
 
-
   // Add commas to arrays for display
-  const partnerDataFormatted = props.projectToEdit.partners.join(", ");
-  const recrutingDataFormatted = props.projectToEdit.recruitingCategories.join(", ");
-  
+  const partnerDataFormatted = props.projectToEdit.partners.join(', ');
+  const recrutingDataFormatted =
+    props.projectToEdit.recruitingCategories.join(', ');
 
   return (
     <div>
-      <div className="project-list-heading">Project: {props.projectToEdit.name}</div>
+      <div className="project-list-heading">
+        Project: {props.projectToEdit.name}
+      </div>
 
       <div>
-        <EditableField 
-          fieldData={props.projectToEdit.name} 
-          fieldName="name" 
+        <EditableField
+          fieldData={props.projectToEdit.name}
+          fieldName="name"
           updateProject={updateProject}
           renderUpdatedProj={props.renderUpdatedProj}
           projId={props.projectToEdit._id}
           fieldType="text"
           fieldTitle="Name:"
+          // accessLevel={props.userAccessLevel}
         />
       </div>
       <div className="editable-field-div">
-        <EditableField 
-          fieldData={props.projectToEdit.description} 
-          fieldName="description" 
+        <EditableField
+          fieldData={props.projectToEdit.description}
+          fieldName="description"
           updateProject={updateProject}
           renderUpdatedProj={props.renderUpdatedProj}
           projId={props.projectToEdit._id}
           fieldType="textarea"
           fieldTitle="Description:"
+          // accessLevel={props.userAccessLevel}
         />
       </div>
       <div className="editable-field-div">
-        <EditableField 
-          fieldData={props.projectToEdit.location} 
-          fieldName="location" 
+        <EditableField
+          fieldData={props.projectToEdit.location}
+          fieldName="location"
           updateProject={updateProject}
           renderUpdatedProj={props.renderUpdatedProj}
           projId={props.projectToEdit._id}
           fieldType="text"
           fieldTitle="Location:"
+          // accessLevel={props.userAccessLevel}
         />
       </div>
       <div className="editable-field-div">
-        <EditableField 
-          fieldData={props.projectToEdit.githubIdentifier} 
-          fieldName="githubIdentifier" 
+        <EditableField
+          fieldData={props.projectToEdit.githubIdentifier}
+          fieldName="githubIdentifier"
           updateProject={updateProject}
           renderUpdatedProj={props.renderUpdatedProj}
           projId={props.projectToEdit._id}
           fieldType="text"
           fieldTitle="GitHub Identifier:"
+          // accessLevel={props.userAccessLevel}
         />
-      </div> 
+      </div>
       <div className="editable-field-div">
-        <EditableField 
-          fieldData={props.projectToEdit.githubUrl} 
-          fieldName="githubUrl" 
+        <EditableField
+          fieldData={props.projectToEdit.githubUrl}
+          fieldName="githubUrl"
           updateProject={updateProject}
           renderUpdatedProj={props.renderUpdatedProj}
           projId={props.projectToEdit._id}
           fieldType="text"
           fieldTitle="GitHib URL:"
+          // accessLevel={props.userAccessLevel}
         />
       </div>
       <div className="editable-field-div">
-        <EditableField 
-          fieldData={props.projectToEdit.slackUrl} 
-          fieldName="slackUrl" 
+        <EditableField
+          fieldData={props.projectToEdit.slackUrl}
+          fieldName="slackUrl"
           updateProject={updateProject}
           renderUpdatedProj={props.renderUpdatedProj}
           projId={props.projectToEdit._id}
           fieldType="text"
           fieldTitle="Slack URL:"
+          // accessLevel={props.userAccessLevel}
         />
       </div>
       <div className="editable-field-div">
-        <EditableField 
-          fieldData={props.projectToEdit.googleDriveUrl} 
-          fieldName="googleDriveUrl" 
+        <EditableField
+          fieldData={props.projectToEdit.googleDriveUrl}
+          fieldName="googleDriveUrl"
           updateProject={updateProject}
           renderUpdatedProj={props.renderUpdatedProj}
           projId={props.projectToEdit._id}
           fieldType="text"
           fieldTitle="Google Drive URL:"
+          accessLevel={props.userAccessLevel}
         />
       </div>
       <div className="editable-field-div">
-        <EditableField 
-          fieldData={props.projectToEdit.googleDriveId} 
-          fieldName="googleDriveId" 
+        <EditableField
+          fieldData={props.projectToEdit.googleDriveId}
+          fieldName="googleDriveId"
           updateProject={updateProject}
           renderUpdatedProj={props.renderUpdatedProj}
           projId={props.projectToEdit._id}
           fieldType="text"
           fieldTitle="Google Drive ID:"
+          // accessLevel={props.userAccessLevel}
         />
       </div>
       <div className="editable-field-div">
-        <EditableField 
-          fieldData={props.projectToEdit.hflaWebsiteUrl} 
-          fieldName="hflaWebsiteUrl" 
+        <EditableField
+          fieldData={props.projectToEdit.hflaWebsiteUrl}
+          fieldName="hflaWebsiteUrl"
           updateProject={updateProject}
           renderUpdatedProj={props.renderUpdatedProj}
           projId={props.projectToEdit._id}
           fieldType="text"
           fieldTitle="HfLA Website URL:"
+          // accessLevel={props.userAccessLevel}
         />
       </div>
       <div className="editable-field-div">
-        <EditableField 
-          fieldData={props.projectToEdit.videoConferenceLink} 
-          fieldName="videoConferenceLink" 
+        <EditableField
+          fieldData={props.projectToEdit.videoConferenceLink}
+          fieldName="videoConferenceLink"
           updateProject={updateProject}
           renderUpdatedProj={props.renderUpdatedProj}
           projId={props.projectToEdit._id}
           fieldType="text"
           fieldTitle="Video Conference Link:"
+          accessLevel={props.userAccessLevel}
         />
       </div>
       <div className="editable-field-div">
-        <EditableField 
-          fieldData={props.projectToEdit.lookingDescription} 
-          fieldName="lookingDescription" 
+        <EditableField
+          fieldData={props.projectToEdit.lookingDescription}
+          fieldName="lookingDescription"
           updateProject={updateProject}
           renderUpdatedProj={props.renderUpdatedProj}
           projId={props.projectToEdit._id}
           fieldType="text"
           fieldTitle="Looking For Description:"
+          // accessLevel={props.userAccessLevel}
         />
       </div>
       <div className="editable-field-div">
-        <EditableField 
-          fieldData={partnerDataFormatted} 
-          fieldName="partners" 
+        <EditableField
+          fieldData={partnerDataFormatted}
+          fieldName="partners"
           updateProject={updateProject}
           renderUpdatedProj={props.renderUpdatedProj}
           projId={props.projectToEdit._id}
           fieldType="text"
           fieldTitle="Partners (comma separated):"
+          accessLevel={props.userAccessLevel}
         />
-      </div> 
+      </div>
       <div className="editable-field-div">
-        <EditableField 
-          fieldData={recrutingDataFormatted} 
-          fieldName="recruitingCategories" 
+        <EditableField
+          fieldData={recrutingDataFormatted}
+          fieldName="recruitingCategories"
           updateProject={updateProject}
           renderUpdatedProj={props.renderUpdatedProj}
           projId={props.projectToEdit._id}
           fieldType="text"
           fieldTitle="Recruiting Categories (comma separated):"
+          // accessLevel={props.userAccessLevel}
         />
-      </div> 
-      
-      
-      <div><button className="button-back" onClick={props.goSelectProject}>Back to Select Project</button></div>
+      </div>
+
+      <div>
+        <button className="button-back" onClick={props.goSelectProject}>
+          Back to Select Project
+        </button>
+      </div>
     </div>
   );
 };
 
-export default EditProjectInfo;
+export default EditProject;
--- client/src/components/manageProjects/editableField.js
+++ client/src/components/manageProjects/editableField.js
@@ -1,72 +1,126 @@
 import React, { useState, useRef, useEffect } from 'react';
 import '../../sass/ManageProjects.scss';
 
-
-
-const EditableField  = (
-    { projId, fieldData, fieldName, updateProject, renderUpdatedProj, fieldType, fieldTitle }
-) => {
+const EditableField = ({
+  projId,
+  fieldData,
+  fieldName,
+  updateProject,
+  renderUpdatedProj,
+  fieldType,
+  fieldTitle,
+  accessLevel,
+}) => {
   const [fieldValue, setFieldValue] = useState(fieldData);
   const [editable, setEditable] = useState(false);
-  const ref = useRef(); 
+  const [userAccessLevel] = useState(accessLevel);
+  const [userCanEdit, setUserCanEdit] = useState(false);
+  const ref = useRef();
+
+  console.log('editableField: props: editable:', editable);
+  console.log('editableFields: props: accessLevel: ', userAccessLevel);
+  console.log('editableFields: props: accessLevel: ', accessLevel);
+  console.log('usercanedit', userCanEdit);
+
+  // create function that checks the user has access to edit all fields
+
+  const canEdit = () => {
+    // get user accessLevel
+    console.log(
+      `editableField: canEdit: activated onClick: editable?: ${editable} : and accessLevel: ${accessLevel}`
+    );
+
+    if (userAccessLevel === 'user') {
+      setUserCanEdit(true);
+    }
+  };
 
   // Update the displayed results to match the change just made to the db
-  useEffect(() => {      
+  useEffect(() => {
+    canEdit();
     if (editable) {
       ref.current.focus();
       setFieldValue(fieldData);
-    } 
-  },[editable]);
+    }
+  }, [editable, userCanEdit]);
 
   return (
+    // here goes the conditional rendering
+    // this button will be disabled if user !admin
     <div>
-      <div>{fieldTitle}<span className="project-edit-button" onClick={() => setEditable(true)} > [edit]</span></div>
-      {editable ?
-      
-        (fieldType === "textarea") ?
+      <div>
+        {fieldTitle}
+        {userCanEdit === true ? (
+          <span
+            className="project-edit-button"
+            onClick={() => {
+              setEditable(true);
+            }}
+          >
+            {' '}
+            [edit]
+          </span>
+        ) : (
+          <span className="project-edit-button" style={{ color: 'gray' }}>
+            {' '}
+            [only certain users can edit this field]
+          </span>
+        )}
+      </div>
+
+      {editable ? (
+        fieldType === 'textarea' ? (
           <textarea
             ref={ref}
             className="editable-field"
-            onBlur={(e)=>{
-              updateProject(projId, fieldName, fieldValue)
-              .then(proj => {renderUpdatedProj(proj); setEditable(false)})
+            onBlur={(e) => {
+              updateProject(projId, fieldName, fieldValue).then((proj) => {
+                renderUpdatedProj(proj);
+                setEditable(false);
+              });
             }}
-            onChange={e=>{
-              setFieldValue(e.target.value)
+            onChange={(e) => {
+              setFieldValue(e.target.value);
               const input = e.target;
               const onEnterKey = (e) => {
                 if (e.keyCode === 13) {
-                  input.removeEventListener('keydown', onEnterKey)
+                  input.removeEventListener('keydown', onEnterKey);
                   input.blur();
                 }
-              }
-              input.addEventListener('keydown', onEnterKey)
+              };
+              input.addEventListener('keydown', onEnterKey);
             }}
-          >{fieldValue}</textarea>
-        :
-        <input 
-          ref={ref}
-          type="text"
-          className="editable-field"
-          value={fieldValue}
-          onBlur={(e)=>{
-            updateProject(projId, fieldName, fieldValue)
-            .then(proj => {renderUpdatedProj(proj); setEditable(false)})
-          }}
-          onChange={e=>{
-            setFieldValue(e.target.value)
-            const input = e.target;
-            const onEnterKey = (e) => {
-              if (e.keyCode === 13) {
-                input.removeEventListener('keydown', onEnterKey)
-                input.blur();
-              }
-            }
-            input.addEventListener('keydown', onEnterKey)
-          }}
-        />
-      : <div className="section-content">{fieldData}</div>
-      }  
+          >
+            {fieldValue}
+          </textarea>
+        ) : (
+          <input
+            ref={ref}
+            type="text"
+            className="editable-field"
+            value={fieldValue}
+            onBlur={(e) => {
+              updateProject(projId, fieldName, fieldValue).then((proj) => {
+                renderUpdatedProj(proj);
+                setEditable(false);
+              });
+            }}
+            onChange={(e) => {
+              setFieldValue(e.target.value);
+              const input = e.target;
+              const onEnterKey = (e) => {
+                if (e.keyCode === 13) {
+                  input.removeEventListener('keydown', onEnterKey);
+                  input.blur();
+                }
+              };
+              input.addEventListener('keydown', onEnterKey);
+            }}
+          />
+        )
+      ) : (
+        <div className="section-content">{fieldData}</div>
+      )}
     </div>
   );
 };
