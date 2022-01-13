fb3ae43733a748e3edb4e5e6fb345293b83c8bb0
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-01-12 20:31:55 -0800

    Created Modal,js, which has local state for a form field that collects the proposed value for an editable field. The component also shows contains the title, project name and name of the field to edit values to create the issue.

--- client/src/components/manageProjects/Modal.js
+++ client/src/components/manageProjects/Modal.js
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Label, Input, AuxiliaryButton } from '../../components/Form';
 
 const Modal = (props) => {
-  console.log(props);
+  const [issue, setIssue] = useState({ proposedValue: '' });
+  console.log('Modal.js: issue: ', issue);
+
+  const handleChange = (e) => {
+    setIssue({ [e.target.name]: e.target.value });
+  };
+
+  const handleCancel = (e) => {
+    setIssue({ proposedValue: '' });
+    props.handleClose(e);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.getIssue(issue);
+    setIssue({ proposedValue: '' });
+  };
+
   return (
     <div>
-      {' '}
-      This is Modal <button onClick={props.handleClose}>close</button>
+      <div>
+        <div onClick={props.handleClose}>Close [X]</div>
+        <p>Title: {props.title}</p>
+        <p>Project Name: {props.project}</p>
+        <p>Field to Edit: {props.fieldToEdit}</p>
+        <form onSubmit={handleSubmit}>
+          <Label>Proposed Value:</Label>
+          <Input
+            type="text"
+            id="proposedValue"
+            name="proposedValue"
+            value={issue.proposedValue}
+            onChange={handleChange}
+          />
+
+          <AuxiliaryButton onClick={handleCancel}>Cancel</AuxiliaryButton>
+          <AuxiliaryButton type="submit">Submit</AuxiliaryButton>
+        </form>
+      </div>
     </div>
   );
 };
