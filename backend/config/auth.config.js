2eca3b64b2018d8c196da64a2c140844904a1b9d
Tim Shaker <tim@tmshkr.com>
2022-03-19 16:31:05 -0700

    increase TOKEN_EXPIRATION_SEC to 7 days

--- backend/config/auth.config.js
+++ backend/config/auth.config.js
@@ -3,6 +3,6 @@ module.exports = {
   SECRET:
     'c0d7d0716e4cecffe9dcc77ff90476d98f5aace08ea40f5516bd982b06401021191f0f24cd6759f7d8ca41b64f68d0b3ad19417453bddfd1dbe8fcb197245079',
   CUSTOM_REQUEST_HEADER: process.env.CUSTOM_REQUEST_HEADER,
-  TOKEN_EXPIRATION_SEC: 900,
+  TOKEN_EXPIRATION_SEC: 60 * 60 * 24 * 7, // 7 days
 };
 /* eslint-enable */
