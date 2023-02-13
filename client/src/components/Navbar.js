18c509acc9b518fb08818bdfdb07fd4e13d4275b
Trillium Smith <spiteless@gmail.com>
2023-02-13 10:07:06 -0800

    feat: Refactor Navbar
    Make all links use the Link element instead of <p> tags
    Clean up return of Nav by moving render logic to sub components

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -7,101 +7,84 @@ import { authLevelRedirect } from '../utils/authUtils'
 import "../sass/Navbar.scss";
 //hooks
 //function -> class
-const Navbar = (props) => {
+const Navbar = ({}) => {
 
   // check user accessLevel and adjust link accordingly
   const [page, setPage] = useState('home');
   const { auth } = useAuth();
   let loginRedirect = '/admin';
+
   if (auth?.user) {
     loginRedirect = authLevelRedirect(auth.user);
+    // admin --> '/admin';
+    // user  --> '/projects';
   }
-  const notAuth = () => (
-    <>
-      {page === 'home' ?
-        <p className="nav-link-text nav-link-active">CHECK-IN</p>
-        :
-        <div className='nav-link-container'>
-          <Link to="/" onClick={e => setPage('home')} >
-            <p className="nav-link-text">
-              CHECK-IN
-            </p>
-          </Link>
-        </div>}
-      {page === 'adminLogin' ?
-        <p className="nav-link-text nav-link-active">ADMIN</p>
-        :
-        <div className='nav-link-container'>
-          <Link to={loginRedirect} onClick={e => setPage('adminLogin')}>
-            <p className="nav-link-text">
-              ADMIN
-            </p>
-          </Link>
-        </div>
-      }
-    </>
-  )
-  const isAuth = () => (
-    <>
-      {page === 'useradmin' ?
-        <p className="nav-link-text nav-link-active" >USER MANAGEMENT</p>
-        :
-        <Link to="/useradmin" onClick={e => setPage('usermanagement')}>
-          <p className="nav-link-text">ADMIN</p>
-        </Link>
-      }
-      {page === 'projects' ?
-        <p className="nav-link-text nav-link-active" >PROJECTS</p>
-        :
-        <Link to="/projects" onClick={e => setPage('projects')}>
-          <p className="nav-link-text">PROJECTS</p>
-        </Link>
-      }
-    </>
-  )
-  const isUser = () => (
-    <>
-      {
-        page === 'home' ?
-          <p className="nav-link-text nav-link-active" >PROJECTS</p>
-          :
-          <Link to="/projects" onClick={e => setPage('home')}>
-            <p className="nav-link-text">PROJECTS</p>
-          </Link>
-      }
-    </>
+
+  const LinkWrapper = ({ text, linkedPage, to }) => {
+    return (
+      <Link
+        className={`nav-link-text ${linkedPage === page && "active"}`}
+        onClick={e => setPage(linkedPage)}
+        to={to}
+      >
+        {text}
+      </Link>
+    )
+  }
+
+  const NoUserLoggedIn = () => {
+    if (!auth?.user) return (
+      <>
+        <LinkWrapper text="CHECK-IN" linkedPage="home" to="/" />
+        <LinkWrapper text="ADMIN" linkedPage="adminLogin" to={loginRedirect} />
+      </>
+    )
+    return (<></>)
+  }
+
+  const AdminLoggedIn = () => {
+    if (auth?.user?.accessLevel === 'admin') return (
+      <>
+        <LinkWrapper text={"HOME"} linkedPage="useradmin" to={loginRedirect} />
+        <LinkWrapper text={"USERS"} linkedPage="usermanagement" to="/useradmin" />
+        <LinkWrapper text={"PROJECTS"} linkedPage="projects" to="/projects" />
+      </>
+    )
+    return (<></>)
+  }
+
+  const UserLoggedIn = () => {
+    if (auth?.user?.accessLevel === 'user') return (
+      <>
+        <LinkWrapper text={"PROJECTS"} linkedPage="projects" to="/projects" />
+      </>
+    )
+    return (<></>)
+  }
+
+  const NavLinks = () => (
+    <div className="navbar-buttons-container">
+      <NoUserLoggedIn />
+      <AdminLoggedIn />
+      <UserLoggedIn />
+    </div>
   )
 
+  const Logo = () => (
+    <div className="navbar-logo">
+      <img src="/hflalogo.png" alt="Hack for LA Logo" />
+    </div>
+  )
 
   return (
     <div className="nav-wrapper">
       <nav className="navbar" role="navigation" aria-label="main navigation">
-        <div className="navbar-buttons-container">
-
-          {!auth?.user && notAuth()}
-
-          {auth?.user?.accessLevel === 'admin' && isAuth()}
-
-          {auth?.user?.accessLevel === 'user' && isUser()}
-
-        </div>
-
-        {props.location.pathname === '/' ||
-          props.location.pathname === '/success' ? (
-          <div className="navbar-logo grow">
-            <img src="/hflalogo.png" alt="Hack for LA Logo"></img>
-          </div>
-        ) : (
-          <div
-            className={`navbar-logo ${props.location.pathname === '/admin' && 'justify-right'
-              }`}
-          >
-            <img src="/hflalogo.png" alt="Hack for LA Logo"></img>
-          </div>
-        )}
+        <NavLinks />
+        <Logo />
       </nav>
     </div>
   );
 };
+
 
 export default withRouter(Navbar);
