481488c469a54338e1cb133f2dbd3a87f02b2bad
Trillium Smith <spiteless@gmail.com>
2023-02-14 15:48:18 -0800

    fix: Remove useEffect import -- no-unused-vars
    in components:
    * UserDashboard
    * UserProfile
    * ReturningUser
    * Auth
    
    Resolves:
    
    * ./src/pages/UserDashboard.js
      * Line 1:17:  'useEffect' is defined but never used  no-unused-vars
    * ./src/pages/UserProfile.js
      * Line 1:17:  'useEffect' is defined but never used  no-unused-vars
    * ./src/pages/ReturningUser.js
      * Line 1:27:  'useEffect' is defined but never used  no-unused-vars
    * ./src/components/auth/Auth.js
      * Line 104:6:  React Hook useEffect has a missing dependency: 'handleLogin'. Either include it or remove the dependency array  react-hooks/exhaustive-deps
      * UseEffect needs to be removed after switch to onSubmit

--- client/src/components/auth/Auth.js
+++ client/src/components/auth/Auth.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import { useHistory } from 'react-router-dom';
 import { checkUser, checkAuth } from '../../services/user.service';
--- client/src/pages/UserDashboard.js
+++ client/src/pages/UserDashboard.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 const UserDashboard = (props) => (
   <div className="flex-container">
