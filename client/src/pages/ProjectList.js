fbb2f9e8f7c2a34be9bda9ac4e7b49dc6bd3f2a2
Matthew Pereira <matthewdavidpereira@gmail.com>
2023-03-16 16:01:47 -0700

    basic styles implimented

--- client/src/pages/ProjectList.js
+++ client/src/pages/ProjectList.js
@@ -1,23 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import ProjectApiService from '../api/ProjectApiService';
-import { Box, CircularProgress, Typography, Container } from '@mui/material';
+import {
+  Box,
+  CircularProgress,
+  Typography,
+  Divider,
+  Button,
+} from '@mui/material';
 import { Link } from 'react-router-dom';
 
 export default function ProjectList() {
   const [projects, setProjects] = useState(null);
   const [projectApiService] = useState(new ProjectApiService());
 
-  /* On component mount, load projects from API */
-  useEffect(function getProjectsOnMount() {
-    console.debug('ProjectList useEffect getProjectsOnMount');
+  /* On component mount, request projects data from API */
+  useEffect(
+    function getProjectsOnMount() {
+      async function fetchAllProjects() {
+        let projectsData = await projectApiService.fetchProjects();
+        //sort the projects alphabetically on front end for now but maybe we can do this on the backend db query
+        projectsData = projectsData.sort((a, b) =>
+          a.name.localeCompare(b.name)
+        );
+        setProjects(projectsData);
+      }
 
-    async function fetchAllProjects() {
-      const projects = await projectApiService.fetchProjects();
-      setProjects(projects);
-    }
-
-    fetchAllProjects();
-  }, []);
+      fetchAllProjects();
+    },
+    [projectApiService]
+  );
 
   /* Render loading circle until project data is served from API */
   if (!projects)
@@ -31,16 +42,46 @@ export default function ProjectList() {
 
   return (
     <Box sx={{ px: 1 }}>
-      <Typography variant="h1" textAlign="center">
-        Project Management
-      </Typography>
-      <p>Here is a list of all projects:</p>
-      <Box sx={{ bgcolor: 'grey.100' }}>
-        {projects.map((project) => (
-          <li key={project._id}>
-            <Link to={`/projects/${project._id}`}>{project.name}</Link>
-          </li>
-        ))}
+      <Box sx={{ my: 3 }}>
+        <Typography variant="h1" textAlign="center">
+          Project Management
+        </Typography>
+      </Box>
+
+      <Box sx={{ textAlign: 'center' }}>
+        <Button
+          component={Link}
+          to="/projects/create"
+          variant="secondary"
+          sx={{ mb: 3 }}
+        >
+          Add a New Project
+        </Button>
+      </Box>
+
+      <Box sx={{ bgcolor: '#F5F5F5' }}>
+        <Box sx={{ p: 2 }}>
+          <Typography sx={{ fontFamily: 'Source Sans Pro', fontWeight: 600 }}>
+            Active Projects
+          </Typography>
+        </Box>
+        <Divider sx={{ borderColor: 'rgba(0,0,0,1)' }} />
+        <Box sx={{ p: 2 }}>
+          {projects.map((project) => (
+            <Box key={project._id} sx={{ mb: 0.5 }}>
+              <Typography
+                sx={{
+                  fontFamily: 'Source Sans Pro',
+                  textTransform: 'uppercase',
+                }}
+                component={Link}
+                to={`/projects/${project._id}`}
+              >
+                {project.name}
+              </Typography>
+            </Box>
+          ))}
+        </Box>
       </Box>
     </Box>
   );
