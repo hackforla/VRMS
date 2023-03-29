50bff0660cc9ad20cfc3837c7e164e7a1eae7921
Matthew Pereira <matthewdavidpereira@gmail.com>
2023-03-29 10:16:34 -0700

    add project name to dropdown and fix checkin bug

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,92 +1,109 @@
-import React, { useState, useEffect } from "react";
-import CheckInButtons from "../components/presentational/CheckInButtons";
-import CreateNewProfileButton from "../components/presentational/CreateNewProfileButton";
-import { REACT_APP_CUSTOM_REQUEST_HEADER as headerToSend } from "../utils/globalSettings";
+import React, { useState, useEffect } from 'react';
+import CheckInButtons from '../components/presentational/CheckInButtons';
+import CreateNewProfileButton from '../components/presentational/CreateNewProfileButton';
+import { REACT_APP_CUSTOM_REQUEST_HEADER as headerToSend } from '../utils/globalSettings';
+import { CircularProgress, Box } from '@mui/material';
 
-import "../sass/Home.scss";
+import '../sass/Home.scss';
 
-const Home = (props) => {
-    const [events, setEvents] = useState([]);
-    // eslint-disable-next-line no-unused-vars
-    const [isLoading, setIsLoading] = useState(false);
-    const [event, setEvent] = useState("--SELECT ONE--");
+const Home = () => {
+  const [events, setEvents] = useState(null);
+  const [selectedEvent, setSelectedEvent] = useState('');
 
+  const handleEventChange = (e) => {
+    setSelectedEvent(e.currentTarget.value);
+  };
+
+  useEffect(() => {
     async function fetchEvents() {
-        try {
-            setIsLoading(true);
-            const res = await fetch("/api/events?checkInReady=true", {
-                    headers: {
-                        "x-customrequired-header": headerToSend
-                    }
-                });
-            const resJson = await res.json();
-            await resJson.unshift("--SELECT ONE--");
+      try {
+        const res = await fetch('/api/events?checkInReady=true', {
+          headers: {
+            'x-customrequired-header': headerToSend,
+          },
+        });
+        const resJson = await res.json();
 
-            setEvents(resJson);
-            setIsLoading(false);
-        } catch (error) {
-            console.log(error);
-            setIsLoading(false);
-        }
+        setEvents(resJson);
+      } catch (error) {
+        console.log(error);
+      }
     }
+    fetchEvents();
+  }, []);
 
-    const handleEventChange = (e) => {
-        setEvent(e.currentTarget.value);
-    };
-
-    useEffect(() => {
-        fetchEvents();
-    }, [event]);
-
+  // render loading spinner until API response
+  if (!events) {
     return (
-            <div className="home">
-                <div className="home-headers">
-                    <h1>VRMS</h1>
-                    <h2>Volunteer Relationship Management System</h2>
-                </div>
-
-                {events && events.length > 1 && (
-                    <div className="meeting-select-container">
-                        <form
-                            className="form-select-meeting"
-                            autoComplete="off"
-                            onSubmit={(e) => e.preventDefault()}
-                        >
-                            <div className="form-row">
-                                <div className="form-input-select">
-                                    <label htmlFor={"meeting-checkin"}>
-                                        Select a meeting to check-in:
-                                    </label>
-                                    <div className="radio-buttons">
-                                        <select
-                                            name={"meeting-checkin"}
-                                            className="select-meeting-dropdown"
-                                            onChange={handleEventChange}
-                                            required
-                                        >
-                                            {events.map((event) => {
-                                                return (
-                                                    <option key={event._id || 0} value={event._id}>
-                                                        {event.name || "--SELECT ONE--"}
-                                                    </option>
-                                                );
-                                            })}
-                                        </select>
-                                    </div>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                )}
-
-                {events.length > 0 && (
-                    <div className="home-buttons">
-                        {event === "--SELECT ONE--" && events.length === 1 && <CreateNewProfileButton />}
-                        {events.length > 1 && <CheckInButtons disabled={event === "--SELECT ONE--"} event={event} events={events}/>}
-                    </div>
-                )}
-            </div>
+      <Box sx={{ display: 'flex', justifyContent: 'center', mt: 15 }}>
+        <CircularProgress />
+      </Box>
     );
+  }
+
+  console.log('EVENTS', events);
+
+  console.log('SELECTED EVENT', selectedEvent);
+
+  return (
+    <div className="home">
+      <div className="home-headers">
+        <h1>VRMS</h1>
+        <h2>Volunteer Relationship Management System</h2>
+      </div>
+
+      {events && events.length > 1 && (
+        <div className="meeting-select-container">
+          <form
+            className="form-select-meeting"
+            autoComplete="off"
+            onSubmit={(e) => e.preventDefault()}
+          >
+            <div className="form-row">
+              <div className="form-input-select">
+                <label htmlFor={'meeting-checkin'}>
+                  Select a meeting to check-in:
+                </label>
+                <div className="radio-buttons">
+                  <select
+                    name={'meeting-checkin'}
+                    className="select-meeting-dropdown"
+                    onChange={handleEventChange}
+                    required
+                    defaultValue="--SELECT ONE--"
+                  >
+                    <option value="--SELECT ONE--" disabled hidden>
+                      --SELECT ONE--
+                    </option>
+                    {events.map((event) => {
+                      return (
+                        <option key={event._id || 0} value={event._id}>
+                          {event?.project?.name + ' - ' + event.name ||
+                            '--SELECT ONE--'}
+                        </option>
+                      );
+                    })}
+                  </select>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      )}
+
+      <div className="home-buttons">
+        {events.length === 0 && <CreateNewProfileButton />}
+
+        {events.length > 0 && (
+          <CheckInButtons
+            disabled={selectedEvent === ''}
+            event={selectedEvent}
+            events={events}
+          />
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
