b06aaa1758da6fdd0f41cfcd1502a723ae4e8dcd
Vanessa Vun <vanessavun@gmail.com>
2023-05-25 16:26:19 -0700

    Removed hard coded switch statements and created one line formula

--- client/src/components/manageProjects/utilities/addDurationToTime.js
+++ client/src/components/manageProjects/utilities/addDurationToTime.js
@@ -1,36 +1,8 @@
 export const addDurationToTime = (startTimeDate, duration) => { 
-	
-	let date = startTimeDate;
-	let endTime;
-
-	// Create the endTime by adding seconds to the timestamp and converting it back to date
-	switch (duration) {
-		case '.5':
-			endTime = new Date(date.getTime() + (.5*3600000)); 
-		break;
-		case '1':
-			endTime = new Date(date.getTime() + (1*3600000)); 
-		break;
-		case '1.5':
-			endTime = new Date(date.getTime() + (1.5*3600000)); 
-		break;
-		case '2':
-			endTime = new Date(date.getTime() + (2*3600000)); 
-			break;
-		case '2.5':
-			endTime = new Date(date.getTime() + (2.5*3600000));
-		break;
-		case '3':
-			endTime = new Date(date.getTime() + (3*3600000));
-		break;
-		case '3.5':
-			endTime = new Date(date.getTime() + (3.5*3600000));
-		break;
-		case '4':
-			endTime = new Date(date.getTime() + (4*3600000));
-		break;
-		default:
-			throw new Error('Error: Cannot calculate endTime');
-	} 
-	return endTime;
+	// Create the endTime by adding seconds based on the selected duration to the timestamp and converting it back to date
+	if(startTimeDate && duration){
+		return new Date(startTimeDate.getTime() + (Number(duration)*3600000))
+	} else {
+		throw new Error('Error: Cannot calculate endTime.')
+	}
 };
