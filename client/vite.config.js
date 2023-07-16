db10b2c905a9d5ed35309e2847b5a063d4f06145
Trillium Smith <spiteless@gmail.com>
2023-07-15 18:01:56 -0700

    feat: Create vite.config.js and mimic create-react-app
    * Add proxy to backend via .env vars
    * Configure output dir to be same as create-react-app

---
+++ client/vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import svgr from "vite-plugin-svgr";
+
+export default defineConfig(({ mode }) => {
+  // Load env file based on`mode in the current working directory.
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [
+      svgr(),
+      react(),
+    ],
+    server: {
+      port: env.CLIENT_PORT,
+      proxy: {
+        '/api': {
+          target: env.REACT_APP_PROXY,
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+    build: {
+      outDir: 'build',
+    },
+  };
+});
