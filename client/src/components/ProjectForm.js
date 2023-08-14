0e359dba899e5f327405da076f5ba652b9573e06
Brad <brad@brads-mbp.lan>
2023-08-13 17:09:25 -0700

    add length validation to description field

--- client/src/components/ProjectForm.js
+++ client/src/components/ProjectForm.js
@@ -37,6 +37,8 @@ const simpleInputs = [
     name: 'description',
     type: 'textarea',
     placeholder: 'Enter project description',
+    value: /^[a-zA-Z0-9 _.,!"'@#$%^&*()/]{0,250}$/,
+    errorMessage: 'Description is too long, max 250 characters allowed'
   },
   {
     label: 'Location',
