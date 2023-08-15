1392b0a0888063d76d5e0b5404af07e7cfe07435
Trillium Smith <spiteless@gmail.com>
2023-08-15 15:26:54 -0700

    fix: Switch config to use process.env.JWT_SECRET

--- backend/config/auth.config.js
+++ backend/config/auth.config.js
@@ -1,7 +1,8 @@
 /*eslint-disable */
+const JWT_SECRET = process.env.JWT_SECRET;
+
 module.exports = {
-  SECRET:
-    'c0d7d0716e4cecffe9dcc77ff90476d98f5aace08ea40f5516bd982b06401021191f0f24cd6759f7d8ca41b64f68d0b3ad19417453bddfd1dbe8fcb197245079',
+  SECRET: JWT_SECRET,
   CUSTOM_REQUEST_HEADER: process.env.CUSTOM_REQUEST_HEADER,
   TOKEN_EXPIRATION_SEC: 900,
 };
