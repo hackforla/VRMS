b9e512d4eb10532b52e116fafac191c3ce4d754e
Phillip Sanchez <plang.psm@gmail.com>
2023-08-22 15:23:38 -0700

    reusable form + additional changes.

--- client/src/components/manageProjects/editProject.jsx
+++ client/src/components/manageProjects/editProject.jsx
@@ -1,64 +1,122 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import EditableField from './editableField';
 import EditMeetingTimes from './editMeetingTimes';
 import CreateNewEvent from './createNewEvent';
 import readableEvent from './utilities/readableEvent';
-import { ReactComponent as EditIcon } from "../../svg/Icon_Edit.svg";
-import '../../sass/ManageProjects.scss';
+import ProjectForm from '../ProjectForm';
+
+import { ReactComponent as EditIcon } from '../../svg/Icon_Edit.svg';
+import { ReactComponent as PlusIcon } from '../../svg/PlusIcon.svg';
+
+import { Typography, Box, Divider } from '@mui/material';
 
 // Need to hold user state to check which type of user they are and conditionally render editing fields in this component
 // for user level block access to all except for the ones checked
 const EditProject = ({
   projectToEdit,
-  userAccessLevel,
-  updateProject,
   recurringEvents,
   createNewRecurringEvent,
   deleteRecurringEvent,
   updateRecurringEvent,
-  regularEvents,
-  updateRegularEvent
 }) => {
-  // Add commas to arrays for display
-  const partnerDataFormatted = projectToEdit.partners.join(', ');
+  const [formData, setFormData] = useState({
+    name: projectToEdit.name,
+    description: projectToEdit.description,
+    location: projectToEdit.location,
+    // githubIdentifier: projectToEdit.name,
+    githubUrl: projectToEdit.githubUrl,
+    slackUrl: projectToEdit.slackUrl,
+    googleDriveUrl: projectToEdit.googleDriveUrl,
+    // hflaWebsiteUrl: projectToEdit.name,
+  });
+
   // eslint-disable-next-line no-unused-vars
-  const recrutingDataFormatted = projectToEdit.recruitingCategories.join(', ');
-  const [recurringEventsState, setRecurringEventsState] = useState([]);
-  const [regularEventsState, setRegularEventsState] = useState([]);
+
+  const [rEvents, setREvents] = useState([]);
   const [selectedEvent, setSelectedEvent] = useState();
   const [isCreateNew, setIsCreateNew] = useState();
 
   // States for alerts
   const [eventAlert, setEventAlert] = useState(null);
 
+  // Form inputs.
+  const simpleInputs = [
+    {
+      label: 'Project Name',
+      name: 'name',
+      type: 'text',
+      value: projectToEdit.name,
+    },
+    {
+      label: 'Project Description',
+      name: 'description',
+      type: 'textarea',
+      value: projectToEdit.description,
+    },
+    {
+      label: 'Location',
+      name: 'location',
+      type: 'text',
+      value: projectToEdit.location,
+    },
+    // Leaving incase we want to add this back in for updating projects
+    // {
+    //   label: 'GitHub Identifier',
+    //   name: 'githubIdentifier',
+    //   type: 'text',
+    //   placeholder: 'Enter GitHub identifier',
+    // },
+    {
+      label: 'GitHub URL',
+      name: 'githubUrl',
+      type: 'text',
+      value: projectToEdit.githubUrl,
+    },
+    {
+      label: 'Slack Channel Link',
+      name: 'slackUrl',
+      type: 'text',
+      value: projectToEdit.slackUrl,
+    },
+    {
+      label: 'Google Drive URL',
+      name: 'googleDriveUrl',
+      type: 'text',
+      value: projectToEdit.googleDriveUrl,
+    },
+    // Leaving incase we want to add this back in for updating projects
+    // {
+    //   label: 'HFLA Website URL',
+    //   name: 'hflaWebsiteUrl',
+    //   type: 'text',
+    //   value: projectToEdit.hflaWebsiteUrl,
+    // },
+  ];
+
   // Get project recurring events when component loads
   useEffect(() => {
-    if (regularEvents) {
-      setRegularEventsState(
-        regularEvents
-          // eslint-disable-next-line no-underscore-dangle
-          .filter((e) => e?.project?._id === projectToEdit._id)
-          .map((item) => ({...item, ...readableEvent(item), raw: item}))
-          .reverse() // sorts most recent events first
-      );
-    }
-  }, [projectToEdit, regularEvents, setRegularEventsState]);
-
-  useEffect(() => {
     if (recurringEvents) {
-      setRecurringEventsState(
+      setREvents(
         recurringEvents
           // eslint-disable-next-line no-underscore-dangle
           .filter((e) => e?.project?._id === projectToEdit._id)
-          .map((item) => ({...item, ...readableEvent(item)}))
+          .map((item) => readableEvent(item))
           .sort((a, b) => a.dayOfTheWeekNumber - b.dayOfTheWeekNumber)
-          );
-        }
-  }, [projectToEdit, recurringEvents, setRecurringEventsState]);
+      );
+    }
+  }, [projectToEdit, recurringEvents, setREvents]);
+
+  // Updates state of formData onChange of any form input
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormData((fData) => ({
+      ...fData,
+      [name]: value,
+    }));
+  };
 
   return (
-    <div>
+    <Box sx={{ px: 0.5 }}>
       <div className={`edit-meeting-modal ${selectedEvent ? 'active' : ''}`}>
         <EditMeetingTimes
           projectToEdit={projectToEdit}
@@ -79,174 +137,73 @@ const EditProject = ({
           setIsCreateNew={setIsCreateNew}
         />
       </div>
-      <Link className="button-back" to={`/projects`}>
-        All Projects
-      </Link>
-      <div className="project-warning-text">
-        *The data here is only test data and is not accurate*
-      </div>
-      <div className="project-list-heading">{`Project: ${projectToEdit.name}`}</div>
-      <EditableField
-        fieldData={projectToEdit.name}
-        fieldName="name"
-        updateProject={updateProject}
-        fieldTitle="Name:"
-        accessLevel={userAccessLevel}
-        canEdit={['admin']}
+      <ProjectForm
+        arr={simpleInputs}
+        formData={formData}
+        projectToEdit={projectToEdit}
+        handleChange={handleChange}
+        isEdit={true}
       />
-      <EditableField
-        fieldData={projectToEdit.description}
-        fieldName="description"
-        updateProject={updateProject}
-        fieldType="textarea"
-        fieldTitle="Description:"
-        accessLevel={userAccessLevel}
-        canEdit={['admin']}
-      />
-      <EditableField
-        fieldData={projectToEdit.location}
-        fieldName="location"
-        updateProject={updateProject}
-        fieldTitle="Location:"
-        accessLevel={userAccessLevel}
-        canEdit={['admin']}
-      />
-      <EditableField
-        fieldData={projectToEdit.githubIdentifier}
-        fieldName="githubIdentifier"
-        updateProject={updateProject}
-        fieldTitle="GitHub Identifier:"
-        accessLevel={userAccessLevel}
-        canEdit={['admin']}
-      />
-      <EditableField
-        fieldData={projectToEdit.githubUrl}
-        fieldName="githubUrl"
-        updateProject={updateProject}
-        fieldTitle="GitHib URL:"
-        accessLevel={userAccessLevel}
-        canEdit={['admin']}
-      />
-      <EditableField
-        fieldData={projectToEdit.slackUrl}
-        fieldName="slackUrl"
-        updateProject={updateProject}
-        fieldTitle="Slack Channel Link:"
-        accessLevel={userAccessLevel}
-        canEdit={['admin']}
-      />
-      <EditableField
-        fieldData={projectToEdit.googleDriveUrl}
-        fieldName="googleDriveUrl"
-        updateProject={updateProject}
-        fieldTitle="Google Drive URL:"
-        accessLevel={userAccessLevel}
-        canEdit={['admin']}
-      />
-      {/* <EditableField   
-        fieldData={projectToEdit.googleDriveId}
-        fieldName="googleDriveId"
-        updateProject={updateProject}
-        fieldTitle="Google Drive ID:"
-        accessLevel={userAccessLevel}
-        canEdit={['admin']}
-      /> */}
-      <EditableField
-        fieldData={projectToEdit.hflaWebsiteUrl}
-        fieldName="hflaWebsiteUrl"
-        updateProject={updateProject}
-        fieldTitle="HfLA Website URL:"
-        accessLevel={userAccessLevel}
-        canEdit={['admin']}
-      />
-      {/* <EditableField
-        fieldData={projectToEdit.videoConferenceLink}
-        fieldName="videoConferenceLink"
-        updateProject={updateProject}
-        fieldTitle="Video Conference Link:"
-        accessLevel={userAccessLevel}
-        canEdit={['admin']}
-      /> */}
-      {/* <EditableField
-        fieldData={projectToEdit.lookingDescription}
-        fieldName="lookingDescription"
-        updateProject={updateProject}
-        fieldTitle="Looking For Description:"
-        accessLevel={userAccessLevel}
-        canEdit={['admin']}
-      /> */}
-      <EditableField
-        fieldData={partnerDataFormatted}
-        fieldName="partners"
-        updateProject={updateProject}
-        fieldTitle="Partners (comma separated):"
-        accessLevel={userAccessLevel}
-        canEdit={['admin']}
-      />
-      {/* <EditableField
-        fieldData={recrutingDataFormatted}
-        fieldName="recruitingCategories"
-        updateProject={updateProject}
-        fieldTitle="Recruiting Categories (comma separated):"
-        accessLevel={userAccessLevel}
-        canEdit={['admin']}
-      /> */}
-      <div className="event-list">
-        <h3>Recurring Events</h3>
-        <h2 className="event-alert">{eventAlert}</h2>
-        <ul>
-          {recurringEventsState.map((event) => (
-            // eslint-disable-next-line no-underscore-dangle
-            <li key={`${event.event_id}`}>
-              <button type="button" onClick={() => setSelectedEvent(event)}>
-                <div>{event.name}</div>
-                <div className="event-list-details">
-                  {`${event.dayOfTheWeek}, ${event.startTime} - ${event.endTime}; ${event.eventType}`}
-                   <div className="edit-icon"><EditIcon /></div>
-                   </div>
-                <div className="event-list-description">{`${event.description}`}</div>
-              </button>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className="display-events">
-        <button
-          type="button"
-          className="create-form-button"
-          onClick={() => setIsCreateNew(true)}
+      <Box sx={{ bgcolor: '#F5F5F5', my: 2 }}>
+        <Box
+          sx={{
+            p: 2,
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }}
         >
-          Create New Event
-        </button>
-      </div>
-
-      <div className="event-list">
-        <h3>Manually Edit Events Checkin</h3>
-        <h2 className="event-alert">{eventAlert}</h2>
-        <ul>
-          {regularEventsState.map((event, index) => (
-            
-            // eslint-disable-next-line no-underscore-dangle
-            <RegularEvent event={event} key={event._id} updateRegularEvent={updateRegularEvent} />
-          ))}
-        </ul>
-      </div>
-    </div>
+          <Box>
+            <Typography sx={{ fontSize: '18px', fontWeight: '600' }}>
+              Recurring Events
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              display: 'flex',
+              '&:hover': { color: 'red', cursor: 'pointer' },
+            }}
+            onClick={() => setIsCreateNew(true)}
+          >
+            <PlusIcon style={{ marginRight: '7px' }} />
+            <Typography
+              sx={{
+                fontSize: '14px',
+                fontWeight: '600',
+              }}
+            >
+              Add New Event
+            </Typography>
+          </Box>
+        </Box>
+        <Divider sx={{ borderColor: 'rgba(0,0,0,1)' }} />
+        <Box sx={{ py: 2, px: 4 }}>
+          <div className="event-list">
+            <h2 className="event-alert">{eventAlert}</h2>
+            <ul>
+              {rEvents.map((event) => (
+                // eslint-disable-next-line no-underscore-dangle
+                <li key={`${event.event_id}`}>
+                  <button type="button" onClick={() => setSelectedEvent(event)}>
+                    <div>{event.name}</div>
+                    <div className="event-list-details">
+                      {`${event.dayOfTheWeek}, ${event.startTime} - ${event.endTime}; ${event.eventType}`}
+                      <div className="edit-icon">
+                        <EditIcon cursor="pointer" />
+                      </div>
+                    </div>
+                    <div className="event-list-description">{`${event.description}`}</div>
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="display-events"></div>
+        </Box>
+      </Box>
+      <Box sx={{ textAlign: 'center' }}></Box>
+    </Box>
   );
 };
-
-function RegularEvent({event, updateRegularEvent}) {
-  return (
-    <li key={`${event.event_id}`}>
-      <button type="button" onClick={async () => updateRegularEvent({checkInReady: !event.checkInReady}, event.event_id)}>
-        <div>{event.name}</div>
-        <div className="event-list-details">
-          {`${event.dayOfTheWeek}, ${event.startTime} - ${event.endTime}; ${event.eventType}`} {`${new Date(event.raw.startTime).toLocaleDateString()}`}
-        </div>
-        <div className="event-list-description">Is this event available for check in now?: <strong>{`${event.checkInReady ? "Yes" : "No"}`}</strong></div>
-      </button>
-    </li>
-  )
-}
 
 export default EditProject;
