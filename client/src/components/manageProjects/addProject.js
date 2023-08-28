4241d9e36008fed0446905ee90fe71095a200340
Brad <brad@brads-mbp.lan>
2023-08-28 11:26:19 -0700

    update description validation

--- client/src/components/manageProjects/addProject.js
+++ client/src/components/manageProjects/addProject.js
@@ -3,6 +3,66 @@ import ProjectForm from '../ProjectForm';
 import { simpleInputs } from '../data';
 
 function addProject() {
+  // Array filled with default inputs.
+  const simpleInputs = [
+    {
+      label: 'Project Name',
+      name: 'name',
+      type: 'text',
+      placeholder: 'Enter project name',
+    },
+    {
+      label: 'Project Description',
+      name: 'description',
+      type: 'textarea',
+      placeholder: 'Enter project description',
+      value: /^[a-zA-Z0-9].{0,250}$/,
+      errorMessage: 'Description must start with alphanumeric characters, 250 char limit'
+    },
+    {
+      label: 'Location',
+      name: 'location',
+      type: 'text',
+      placeholder: 'Enter location for meeting',
+      value: /https:\/\/[\w-]*\.?zoom.us\/(j|my)\/[\d\w?=-]+/,
+      errorMessage: 'Please enter a valid Zoom URL',
+      addressValue: '',
+      addressError: 'Invalid address',
+    },
+    // Leaving incase we want to add this back in for updating projects
+    // {
+    //   label: 'GitHub Identifier',
+    //   name: 'githubIdentifier',
+    //   type: 'text',
+    //   placeholder: 'Enter GitHub identifier',
+    // },
+    {
+      label: 'GitHub URL',
+      name: 'githubUrl',
+      type: 'text',
+      placeholder: 'htttps://github.com/',
+    },
+    {
+      label: 'Slack Channel Link',
+      name: 'slackUrl',
+      type: 'text',
+      placeholder: 'htttps://slack.com/',
+    },
+    {
+      label: 'Google Drive URL',
+      name: 'googleDriveUrl',
+      type: 'text',
+      placeholder: 'htttps://drive.google.com/',
+    },
+    // Leaving incase we want to add this back in for updating projects
+    // {
+    //   label: 'HFLA Website URL',
+    //   name: 'hflaWebsiteUrl',
+    //   type: 'text',
+    //   placeholder: 'htttps://hackforla.org/projects/',
+    // },
+  ];
+
   return (
     <div>
       <ProjectForm
--- client/src/components/manageProjects/editProject.js
+++ client/src/components/manageProjects/editProject.js
@@ -60,6 +60,65 @@ const EditProject = ({
     }
   }, [projectToEdit, regularEvents, setRegularEventsState]);
 
+  // Form inputs.
+  const simpleInputs = [
+    {
+      label: 'Project Name',
+      name: 'name',
+      type: 'text',
+      value: projectToEdit.name,
+    },
+    {
+      label: 'Project Description',
+      name: 'description',
+      type: 'textarea',
+      value: projectToEdit.description,
+      value: /^[a-zA-Z0-9].{0,250}$/,
+      errorMessage: 'Description must start with alphanumeric characters, 250 char limit'
+    },
+    {
+      label: 'Location',
+      name: 'location',
+      type: 'text',
+      value: /https:\/\/[\w-]*\.?zoom.us\/(j|my)\/[\d\w?=-]+/,
+      errorMessage: 'Please enter a valid Zoom URL',
+      addressValue: '',
+      addressError: 'Invalid address',
+    },
+    // Leaving incase we want to add this back in for updating projects
+    // {
+    //   label: 'GitHub Identifier',
+    //   name: 'githubIdentifier',
+    //   type: 'text',
+    //   placeholder: 'Enter GitHub identifier',
+    // },
+    {
+      label: 'GitHub URL',
+      name: 'githubUrl',
+      type: 'text',
+      value: projectToEdit.githubUrl,
+    },
+    {
+      label: 'Slack Channel Link',
+      name: 'slackUrl',
+      type: 'text',
+      value: projectToEdit.slackUrl,
+    },
+    {
+      label: 'Google Drive URL',
+      name: 'googleDriveUrl',
+      type: 'text',
+      value: projectToEdit.googleDriveUrl,
+    },
+    // Leaving incase we want to add this back in for updating projects
+    // {
+    //   label: 'HFLA Website URL',
+    //   name: 'hflaWebsiteUrl',
+    //   type: 'text',
+    //   value: projectToEdit.hflaWebsiteUrl,
+    // },
+  ];
+
   // Get project recurring events when component loads
   useEffect(() => {
     if (recurringEvents) {
