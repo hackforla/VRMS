5f6fd39cf956b27842577875c423dadaec5d02ae
Trillium S <Spiteless@gmail.com>
2023-09-06 18:39:53 -0700

    Revert "feat: Whitelist 'onboarding' in event validation"

--- client/src/components/manageProjects/utilities/validateEventForm.js
+++ client/src/components/manageProjects/utilities/validateEventForm.js
@@ -1,6 +1,5 @@
 import validator from 'validator';
 import { isWordInArrayInString } from './../../../utils/stringUtils.js';
-import { el } from 'date-fns/locale';
 
 const validateEventForm = (vals, projectToEdit) => {
   let newErrors = {};
@@ -26,14 +25,10 @@ const validateEventForm = (vals, projectToEdit) => {
             vals[key].toLowerCase()
           )
         ) {
-          if (projectToEdit.name.toLowerCase() === 'onboarding') {
-            // Do nothing, word `onboarding` has been white-listed
-          } else {
-            newErrors = {
-              ...newErrors,
-              name: `Event name cannot contain the Project Name: '${projectToEdit.name}'`,
-            };
-          }
+          newErrors = {
+            ...newErrors,
+            name: `Event name cannot contain the Project Name: '${projectToEdit.name}'`,
+          };
         }
         break;
 
