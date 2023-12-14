edc8070f819fe121951f7b3adb8994f6ab69f168
freaky4wrld <freaky4wrld@gmail.com>
2023-12-14 15:40:44 +0530

    fix: added some new properties to the index.js and removed ThemeProvider from Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,62 +6,7 @@ import { CircularProgress, Box, Typography } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import '../sass/Home.scss';
-import aliseowoff2 from '../fonts/aliseo-noncommercial-webfont.woff2';
 
-const fontTheme = createTheme({
-  typography: {
-    fontFamily: ['aliseoregular',
-     '-apple-system',
-      'BlinkMacSystemFont', 
-      'Segoe UI',
-      'Roboto',
-      'Oxygen',
-      'Ubuntu',
-      'Cantarell',
-      '"Fira Sans"',
-      '"Droid Sans"',
-      '"Helvetica Neue"',
-      'sans-serif',].join(','),
-    h1: {
-      fontSize: '7rem',
-      lineHeight: '0.85rem',
-      letterSpacing: '0.05rem',
-      '@media (min-width:500px)': {
-        fontSize: '6rem',
-        lineHeight: '0.75rem',
-      },
-    },
-    h2: {
-      fontSize: '2.8rem',
-      lineHeight: '1.05rem',
-      letterSpacing: '0.025rem',
-      '@media (min-width:500px)': {
-        fontSize: '2.7rem',
-        lineHeight: '1rem',
-      },
-    },
-    h4: {
-      fontSize: '2rem',
-      '@media (min-width:500px)': {
-        fontSize: '2rem',
-      },
-    },
-  },
-  components: {
-    MuiCssBaseline: {
-      styleOverrides: `
-        @font-face {
-          font-family: 'aliseoregular';
-          font-style: normal;
-          font-display: swap;
-          font-weight: 400;
-          src: local('aliseoregular'), local('aliseoregular'), url(${aliseowoff2}) format('woff2');
-          unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
-        }
-      `,
-    },
-  },
-});
 
 const Home = () => {
   const [events, setEvents] = useState(null);
@@ -100,14 +45,13 @@ const Home = () => {
   }
 
   return (
-    <ThemeProvider theme={fontTheme}>
       <Box className="home">
         <Box className="home-headers">
           <Typography variant='h1'>VRMS</Typography>
           <Typography variant='h2'>Volunteer Relationship Management System</Typography>
         </Box>
 
-        {events && events.length > 0 ? (
+        {false ? (
           <Box className="meeting-select-container">
             <form
               className="form-select-meeting"
@@ -163,7 +107,6 @@ const Home = () => {
             </Box>  
           )}
       </Box>
-    </ThemeProvider>
   );
 };
 
