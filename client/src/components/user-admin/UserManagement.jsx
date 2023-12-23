1517395644721e0babca1b02af2b4542ed8ce1cb
freaky4wrld <freaky4wrld@gmail.com>
2023-12-23 16:19:16 +0530

    fix+feat:refractored the current code in accordance to MUI component and added New user button

--- client/src/components/user-admin/UserManagement.jsx
+++ client/src/components/user-admin/UserManagement.jsx
@@ -1,5 +1,16 @@
 import React, { useState } from 'react';
+import {Box, Button, ButtonGroup, TextField,Typography } from '@mui/material';
+
+
 import '../../sass/UserAdmin.scss';
+
+const h3sx = {
+  fontFamily: 'aliseoregular',
+  fontSize: {xs: "1.6rem"},
+  marginBottom: `1rem`,
+  marginTop: `1rem`,
+  textAlign: "center",
+}
 
 const UserManagement = ({ users, setUserToEdit }) => {
   let searchResults = [];
@@ -44,64 +55,77 @@ const UserManagement = ({ users, setUserToEdit }) => {
   return (
     <div className="container--usermanagement">
       <div>
-        <h3>User Management</h3>
-        <input
+        <Typography variant='h3' sx={h3sx}>User Management</Typography>
+        
+        <div className="tab-buttons">
+          <ButtonGroup>
+            <div>
+              <Button
+                type="button"
+                className={
+                  searchResultType === 'name'
+                    ? 'select-button selected'
+                    : 'select-button'
+                }
+                variant='secondary'
+                onClick={buttonSwap}
+                disabled={searchResultType === 'name'}
+              >
+                Results by Name
+              </Button>
+            </div>
+            <div>
+              <Button
+                type="button"
+                className={
+                  searchResultType === 'email'
+                    ? 'select-button selected'
+                    : 'select-button'
+                }
+                variant='secondary'
+                onClick={buttonSwap}
+                disabled={searchResultType === 'email'}
+              >
+                Results by Email
+              </Button>
+            </div>
+          </ButtonGroup>
+        </div>
+        <TextField
           type="text"
           placeholder="Search by name and email..."
           value={searchTerm}
           onChange={handleChange}
         />
-        <div className="tab-buttons">
+        <Box>
           <div>
-            <button
-              type="button"
-              className={
-                searchResultType === 'name'
-                  ? 'select-button selected'
-                  : 'select-button'
-              }
-              onClick={buttonSwap}
-              disabled={searchResultType === 'name'}
-            >
-              Results by Name
-            </button>
+              <ul className="search-results">
+                {searchResults.map((u) => {
+                  return (
+                    // eslint-disable-next-line no-underscore-dangle
+                    <li key={`result_${u._id}`}>
+                      <button
+                        className="search-results-button"
+                        type="button"
+                        onClick={() => setUserToEdit(u)}
+                      >
+                        {searchResultType === 'name'
+                          ? `${u.name?.firstName} ${u.name?.lastName} ( ${u.email} )`
+                          : `${u.email} ( ${u.name?.firstName} ${u.name?.lastName} )`}
+                      </button>
+                    </li>
+                  );
+                })}
+              </ul>
           </div>
-          <div>
-            <button
-              type="button"
-              className={
-                searchResultType === 'email'
-                  ? 'select-button selected'
-                  : 'select-button'
-              }
-              onClick={buttonSwap}
-              disabled={searchResultType === 'email'}
-            >
-              Results by Email
-            </button>
-          </div>
-        </div>
+        </Box>
         <div>
-          <div>
-            <ul className="search-results">
-              {searchResults.map((u) => {
-                return (
-                  // eslint-disable-next-line no-underscore-dangle
-                  <li key={`result_${u._id}`}>
-                    <button
-                      className="search-results-button"
-                      type="button"
-                      onClick={() => setUserToEdit(u)}
-                    >
-                      {searchResultType === 'name'
-                        ? `${u.name?.firstName} ${u.name?.lastName} ( ${u.email} )`
-                        : `${u.email} ( ${u.name?.firstName} ${u.name?.lastName} )`}
-                    </button>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
+          <Button
+            type='button'
+            variant='secondary'
+          >
+            Add a New User
+          </Button>
         </div>
       </div>
     </div>
