cf0ba9187ac26b38ca766aeb931a09d9b7638345
Trillium Smith <spiteless@gmail.com>
2024-02-09 15:41:07 -0800

    fix: Update generateEventData to handle daylight savings * Move generateEventData to its own file * Add tests for generateEventData Co-authored-by: danieldkim42 <danieldkim42@users.noreply.github.com>

--- backend/workers/createRecurringEvents.js
+++ backend/workers/createRecurringEvents.js
@@ -75,43 +75,6 @@ module.exports = (cron, fetch) => {
         };
     };
 
-    function generateEventData(eventData, TODAY_DATE = new Date()) {
-        const eventDate = new Date(eventData.startTime);
-        // Create new event
-        const hours = eventDate.getHours();
-        const minutes = eventDate.getMinutes();
-        const seconds = eventDate.getSeconds();
-        const milliseconds = eventDate.getMilliseconds();
-
-        const yearToday = TODAY_DATE.getFullYear();
-        const monthToday = TODAY_DATE.getMonth();
-        const dateToday = TODAY_DATE.getDate();
-
-        const newEventDate = new Date(yearToday, monthToday, dateToday, hours, minutes, seconds, milliseconds);
-
-        const newEndTime = new Date(yearToday, monthToday, dateToday, hours + filteredEvent.hours, minutes, seconds, milliseconds)
-
-        const eventToCreate = {
-            name: eventData.name && eventData.name,
-            hacknight: eventData.hacknight && eventData.hacknight,
-            eventType: eventData.eventType && eventData.eventType,
-            description: eventData.eventDescription && eventData.eventDescription,
-            project: eventData.project && eventData.project,
-            date: eventData.date && newEventDate,
-            startTime: eventData.startTime && newEventDate,
-            endTime: eventData.endTime && newEndTime,
-            hours: eventData.hours && eventData.hours
-        }
-        if (eventData.hasOwnProperty("location")) {
-            eventToCreate.location = {
-                city: eventData.location.city ? eventData.location.city : 'REMOTE',
-                state: eventData.location.state ? eventData.location.state : 'REMOTE',
-                country: eventData.location.country ? eventData.location.country : 'REMOTE'
-            };
-        }
-        return eventToCreate
-    };
-
     async function checkIfEventExists(eventName) {
         const events = EVENTS;
         // const today = new Date();
