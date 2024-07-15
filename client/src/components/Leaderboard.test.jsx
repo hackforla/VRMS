7516f27c93dd12bef6ed78d112b68f25468769bb
Trillium Smith <spiteless@gmail.com>
2024-07-15 13:35:16 -0700

    feat: Update from depricated react-test-renderer to @testing-library/react in /client

--- client/src/components/Leaderboard.test.jsx
+++ client/src/components/Leaderboard.test.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import LeaderBoard from "./Leaderboard";
-import renderer from "react-test-renderer";
-import { test, expect } from 'vitest'
+import { render } from "@testing-library/react";
+import { test, expect } from 'vitest';
 
 test("renders without crashing", () => {
-  const tree = renderer.create(<LeaderBoard />).toJSON();
-  expect(tree).toMatchSnapshot();
+  const { container } = render(<LeaderBoard />);
+  expect(container).toMatchSnapshot();
 });
