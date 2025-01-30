3a25f7f00a2dc8d183e11f27cd58f343732f0e3b
JamesNg <njames15@gmail.com>
2025-01-29 21:38:43 -0500

    updated HTML span and img tags to MUI typography and box components #1719

--- client/src/pages/ManageProjects.jsx
+++ client/src/pages/ManageProjects.jsx
@@ -8,12 +8,35 @@ import RecurringEventsApiService from '../api/RecurringEventsApiService';
 import Loading from '../svg/22.gif';
 import '../sass/ManageProjects.scss';
 import EventsApiService_ from '../api/EventsApiService';
+import { Typography, Box } from '@mui/material';
 
 const PAGES = Object.freeze({
   selectProject: 'selectProject',
   editProjectInfo: 'editProjectInfo',
   editMeetingTimes: 'editMeetingTimes',
 });
+
+// Added styles for MUI components
+const loadingStyle = {
+  "display": "none",
+  "position": "absolute",
+  "display": "flex",
+  "flexDirection": "row",
+  "alignItems": "center",
+  "justifyContent": "center",
+  "zIndex": 1,
+  "top": 0,
+  "left": 0,
+  "right": 0,
+  "backgroundColor": "white",
+  "height": '100%',
+  "opacity": 0.6,
+}
+
+const noStyle = {
+  "opacity": 0,
+  "display": "none",
+}
 
 const ManageProjects = () => {
   const { projectId } = useParams();
@@ -155,13 +178,13 @@ const ManageProjects = () => {
   }
   return (
     <>
-      <span
-        className={`bg-overlay ${
-          eventsLoading || projectsLoading ? 'active' : ''
-        }`}
+      <Typography
+        sx={eventsLoading || projectsLoading ? loadingStyle : noStyle}
+        component='span'
+        display='inline'
       >
-        <img src={Loading} alt="Logo" />
-      </span>
+        <Box component='img' src={Loading} alt="Logo" />
+      </Typography>
       {displayedComponent}
     </>
   );
