f260bd6a9a22d7d454a653d132fc21ea04c10344
JamesNg <njames15@gmail.com>
2025-05-13 14:22:05 -0400

    wrote unit tests for Auth router

--- backend/routers/auth.router.test.js
+++ backend/routers/auth.router.test.js
@@ -29,6 +29,8 @@ const { UserController, EmailController } = require('../controllers');
 const express = require('express');
 const supertest = require('supertest');
 const authRouter = require('../routers/auth.router');
+const { verifyToken, verifyUser, AuthUtil } = require('../middleware');
+const { authApiValidator } = require('../validators');
 
 // Create a new Express application for testing
 const testapp = express();
@@ -36,6 +38,7 @@ const testapp = express();
 testapp.use(express.json());
 testapp.use('/api/auth', authRouter);
 const request = supertest(testapp);
+
 
 describe('Unit tests for auth router', () => {
   // Clear all mocks after each test
@@ -71,6 +74,8 @@ describe('Unit tests for auth router', () => {
       });
 
       // Tests
+      expect(authApiValidator.validateCreateUserAPICall).toHaveBeenCalled();
+      expect(verifyUser.checkDuplicateEmail).toHaveBeenCalled();
       expect(UserController.createUser).toHaveBeenCalled();
       expect(response.status).toBe(201);
       expect(response.body).toEqual({ message: 'User created successfully' });
@@ -105,7 +110,7 @@ describe('Unit tests for auth router', () => {
           headers.origin,
         );
 
-        res.status(200).send({ message: 'Signin successful' });
+        res.status(200).send('Signin successful');
       });
 
       // Mock implementation for EmailController.sendLoginLink
@@ -119,6 +124,8 @@ describe('Unit tests for auth router', () => {
       });
 
       // Tests
+      expect(authApiValidator.validateSigninUserAPICall).toHaveBeenCalled();
+      expect(verifyUser.isAdminByEmail).toHaveBeenCalled();
       expect(UserController.signin).toHaveBeenCalled();
       expect(EmailController.sendLoginLink).toHaveBeenCalledWith(
         email,
@@ -131,10 +138,76 @@ describe('Unit tests for auth router', () => {
       expect(response.status).toBe(200);
       // Verify that the cookie is set
       expect(response.headers['set-cookie']).toBeDefined();
-      expect(response.headers['set-cookie'][0]).toContain('token=mockedCookie');
+      expect(response.headers['set-cookie'][0]).toContain(`token=${cookie}`);
+      expect(response.text).toBe('Signin successful');
 
       // Marks completion of tests
       done();
     });
+
+    it('should verify sign in with POST /api/auth/verify-signin', async (done) => {
+      // Mock implementation for UserController.verifySignIn
+      UserController.verifySignIn.mockImplementation((req, res) => {
+        res.status(200).send(mockUser);
+      });
+
+      // Mock POST API call
+      const response = await request.post('/api/auth/verify-signin').send({
+        token: 'mockedToken',
+      });
+
+      // Tests
+      expect(verifyToken.isTokenValid).toHaveBeenCalled();
+      expect(UserController.verifySignIn).toHaveBeenCalled();
+      expect(response.status).toBe(200);
+      expect(response.body).toEqual(mockUser);
+
+      // Marks completion of tests
+      done();
+    });
+
+    it('should verify me with POST /api/auth/me', async (done) => {
+      // Mock implementation for UserController.verifyMe
+      UserController.verifyMe.mockImplementation((req, res) => {
+        res.status(200).send(mockUser);
+      });
+
+      // Mock POST API call
+      const response = await request.post('/api/auth/me').send({
+        token: 'mockedToken',
+      });
+
+      // Tests
+      expect(AuthUtil.verifyCookie).toHaveBeenCalled();
+      expect(UserController.verifyMe).toHaveBeenCalled();
+      expect(response.status).toBe(200);
+      expect(response.body).toEqual(mockUser);
+
+      // Marks completion of tests
+      done();
+    });
+
+    it('should log out with POST /api/auth/logout', async (done) => {
+      const token = 'token';
+      // Mock implementation for UserController.logout
+      UserController.logout.mockImplementation((req, res) => {
+        res.clearCookie(token);
+        res.status(200).send('Successfully logged out.');
+      });
+  
+      // Mock POST API call
+      const response = await request.post('/api/auth/logout').set('Cookie', token);
+  
+      // Tests
+      expect(AuthUtil.verifyCookie).toHaveBeenCalled();
+      expect(UserController.logout).toHaveBeenCalled();
+      expect(response.headers['set-cookie'][0]).toMatch(/token=;/);
+      expect(response.status).toBe(200);
+      expect(response.text).toBe('Successfully logged out.');
+  
+      // Marks completion of tests
+      done();
+    });
   });
+
 });
