1af73547522a090d5e353993bb5bf007d1efc0ad
Trillium Smith <trillium@trilliumsmith.com>
2025-06-26 10:40:04 -0700

    ci: Add build SHA to health check for deployment verification

--- backend/controllers/healthCheck.controller.js
+++ backend/controllers/healthCheck.controller.js
@@ -1,7 +1,9 @@
 const HealthCheckController = {};
 
 HealthCheckController.isAlive = (_, res) => {
-  res.status(200).send("I'm Alive!");
-}
+  res
+    .status(200)
+    .send(`I'm Alive! Build: ${process.env.BUILD_SHA || 'unknown'} - ${new Date().toISOString()}`);
+};
 
 module.exports = HealthCheckController;
