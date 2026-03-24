db8a989f81dfe709902d10a032b3a9a7b5c47860
Kirk Chu <kkchu791@gmail.com>
2026-03-23 21:19:04 -0700

    adding package dependency fix

--- client/src/pages/UserWelcome.jsx
+++ client/src/pages/UserWelcome.jsx
@@ -11,12 +11,12 @@ export default function UserWelcome() {
 
   const firstName = user?.name.firstName;
 
-  console.log('AUTH', auth);
-  console.log(flags, 'flags', 'we have user logged in at this point');
   console.log(user, 'user');
   return (
     <Box textAlign="center" sx={{ pt: 5 }}>
-      <Typography variant="h1">Welcome {firstName}!</Typography>
+      <Typography variant="h1">
+        Welcome {flags.cool_welcome_message && 'Sir'} {firstName}!
+      </Typography>
       <Box sx={{ fontSize: '16px' }}>
         <Typography variant="p">For assistance using VRMS, check out the </Typography>
         <Link
