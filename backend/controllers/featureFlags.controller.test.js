306258d83ffd776c35f42f425fb6a938abbade81
Kirk Chu <kkchu791@gmail.com>
2026-03-30 20:24:16 -0700

    add another test

--- backend/controllers/featureFlags.controller.test.js
+++ backend/controllers/featureFlags.controller.test.js
@@ -1,17 +1,36 @@
 const FeatureFlagsController = require('./featureFlags.controller');
 
-test('can get feature flags', async () => {
-  const req = {
-    headers: { 'x-customrequired-header': 'secret-header' },
-    userId: 'user-1',
-  };
+const { getAllFlags } = require('../services/featureFlags.service');
 
-  const res = {
-    status: jest.fn().mockReturnThis(),
-    json: jest.fn(),
-  };
+jest.mock('../services/featureFlags.service.js');
 
-  await FeatureFlagsController.index(req, res);
+describe('FeatureFlagsController.index', () => {
+  let req, res;
 
-  expect(res.status).toHaveBeenCalledWith(200);
+  beforeEach(() => {
+    jest.spyOn(console, 'error').mockImplementation(() => {});
+    req = {
+      headers: { 'x-customrequired-header': process.env.CUSTOM_REQUEST_HEADER },
+      userId: 'user-1',
+    };
+    res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+      sendStatus: jest.fn(),
+    };
+  });
+
+  test('can get feature flags', async () => {
+    await FeatureFlagsController.index(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(200);
+  });
+
+  test('should return 400 when getAllFlags throws an error', async () => {
+    getAllFlags.mockRejectedValue(new Error('error'));
+
+    await FeatureFlagsController.index(req, res);
+
+    expect(res.sendStatus).toHaveBeenCalledWith(400);
+  });
 });
