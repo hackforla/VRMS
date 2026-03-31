3fc324576640939fa6e496acbdaf4e815170e71f
Richy Teas <richyteas@gmail.com>
2026-03-30 19:08:02 -0700

    addressing bryant's changes to use proper import/module loading syntax

--- backend/routers/projects.router.js
+++ backend/routers/projects.router.js
@@ -9,7 +9,7 @@ const { ProjectController } = require('../controllers');
 // router.use(Auth.authUser, Auth.requireMinimumRole(ROLES.PROJECT_MANAGER));
 // The base is /api/projects
 
-import { AuthUtil } from '../middleware/auth.middleware';
+const { AuthUtil } = require('../middleware/auth.middleware');
 
 router.get('/', ProjectController.project_list);
 
